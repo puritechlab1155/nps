2215121561123f9497b88f077898d831ac706582
puritech <pub@puritechlab.com>
2024-06-18 19:19:03 +0900

    헬스 앤 라이프

--- html/vol200/sub08.php
+++ html/vol200/sub08.php
@@ -25,22 +25,22 @@
 
 		<div class="section02 wow fadeBigInUp">
 			<div class="content">
-				<div class="col1 fadeBigInUp">
+				<div class="fadeBigInUp">
 					<div class="subtitle">조금만 움직여도 땀이 뚝뚝~</div>
 					<div class="title">여름철 다한증이 고민된다면?</div>
+					<div class="line"></div>
 					<div class="headcopy pc_display">
 						시험을 볼 때 시험지가 다 젖거나, 다른 사람과 악수를 꺼리거나, 발냄새로 쉽게 신발을 벗지 못했던 기억이 떠오른다면 다한증을 의심해 볼만 하다. 심리적으로 신체적으로 불편함을 유발하는 다한증에 대해 알아본다.
 					</div>
 					<div class="headcopy mobile_display">
 						시험을 볼 때 시험지가 다 젖거나, 다른 사람과 악수를 꺼리거나, 발냄새로 쉽게 신발을 벗지 못했던 기억이 떠오른다면 다한증을 의심해 볼만 하다. 심리적으로 신체적으로 불편함을 유발하는 다한증에 대해 알아본다.
 					</div>
-					<div class="line"></div>
 					<div class="subcopy">
 						<b>필요 이상으로 땀이 흐른다면</b><br>
-						다한증은 필요 이상으로 열이 발생해 특정 부위에 비정상적으로 땀을 흘리는 질환이다. 보통 사계절 내내 증상이 나타나지만, 여름은 무더위라는 고온 자극 때문에 다한증 환자들에게 가장 고통스러운 계절이다. 종류도 다양하다. 손발에 증상이 나타나는 수족 다한증, 수면 중 땀이 많이 나는 도한증, 겨드랑이에 땀이 많이 나는 액한증 등이다. 유독 땀이 많이 나서 여름이 두렵다면, 아래의 다한증 자가 진단 테스트를 해보자. 
+						  다한증은 필요 이상으로 열이 발생해 특정 부위에 비정상적으로 땀을 흘리는 질환이다. 보통 사계절 내내 증상이 나타나지만, 여름은 무더위라는 고온 자극 때문에 다한증 환자들에게 가장 고통스러운 계절이다. 종류도 다양하다. 손발에 증상이 나타나는 수족 다한증, 수면 중 땀이 많이 나는 도한증, 겨드랑이에 땀이 많이 나는 액한증 등이다. 유독 땀이 많이 나서 여름이 두렵다면, 아래의 다한증 자가 진단 테스트를 해보자.
 					</div>
 				</div>
-				<div class="col2 fadeBigInUp">
+				<div class="fadeBigInUp">
 					<img src="img/sub08/sub08_02.svg">
 				</div>
 			</div>
@@ -48,53 +48,55 @@
 
 		<div class="section03 wow fadeBigInUp">
 			<div class="content">
-				<div class="title fadeBigInUp">
-					<img src="img/sub08/sub08_03.svg">
-					<div class="female_img mobile_display"><img src="img/sub08/sub08_04.png"></div>
+
+			    <div class="subcopy">
+					<b>어디서 이렇게 땀이 나나? 부위별로 다른 ‘다한증’</b><br>
+					 머리, 얼굴, 손발, 겨드랑이 등 어느 한 부위에 유독 땀이 많이 난다면 건강 상태를 점검할 필요가 있다. 이는 땀을 지나치게 많이 흘리는 ‘다한증’의 주요 증상이기 때문이다. 여름이면 더욱 심해지는 다한증을 부위별로 살펴본다. 
 				</div>
-				<div class="item1 fadeBigInUp">
-					<li class="pc_display left">
-						<div class="female_img"><img src="img/sub08/sub08_04.png"></div>
-					</li>
-					<li>
-						<div class="circle_img" id="circle1"><img src="img/sub08/sub08_05.svg"></div>
-						<div class="item_title line01">손톱 끝이 쉽게 갈라지고</div>
-						<div class="item_title line02">부서진다면?</div>
-						<div class="item_text">손톱이 갈라지거나 부스러지는 현상은 혈액 순환이 원활하지 않고 영양 상태가 나쁘기 때문에 발생한다. 따라서 영양을 보충해 줄 수 있는 음식을 섭취하는 것이 중요하다. 육류와 달걀, 우유 등으로 단백질을 보충해주고 각종 비타민이 많이 함유된 채소류와 통곡물을 먹는 것이 좋다.</div>
-					</li>
-					<div class="line mobile_display"></div>
+
+			    <div class="all_col">
+
+			    <div class="col1 fadeBigInUp">
+				  <!-- <div class="item1 fadeBigInUp"> -->
+					<!-- <li class="pc_display left"> -->
+					    <div class="arm_foot_img"><img src="img/sub08/sub08_03.png"></div>
+					<!-- </li> -->
+					<!-- <li> -->
+						<div class="circle_img" ><img src="img/sub08/sub08_04.png"></div>
+						<!-- <div class="item_title line01">손톱 끝이 쉽게 갈라지고</div>
+						<div class="item_title line02">부서진다면?</div> -->
+						<div class="item_text">사람들은 긴장하면 손발에 땀이 난다. 이는 긴장 상태를 해소하는 현상으로 시간이 지나면 마른다. 반면, 수족 다한증은 빠른 시간에 모든 땀이 쏟아져 손발이 전부 젖어버리게 된다. 학생이 손발에 땀이 난다면 학업적 불편함과 함께 땀으로 유발되는 발냄새로 인해 교우 관계에 불편함이 나타날 수 있다. 성인의 경우 컴퓨터 작업이나 수작업 등의 정교한 업무가 힘들어질 수 있고, 악수하는 상황에서 손이 축축이 젖어 심리적인 위축이 나타날 수 있다.</div>
+					<!-- </li> -->
+					    <div class="line mobile_display"></div>
 				</div>
-				<div class="item2 fadeBigInUp">
-					<li class="left">
-						<div class="circle_img" id="circle2"><img src="img/sub08/sub08_06.svg"></div>
-						<div class="item_title line01">손톱의 색이</div>
-						<div class="item_title line02">유난히 노랗다면?</div>
-						<div class="item_text">자신의 손톱이 유난히 노랗게 보인다면 손톱 끝의 두께를 한번 확인해 보자. 만약 손톱의 끝이 오므라들고 손톱의 두께가 얇아졌거나 두꺼워지는 현상이 동시에 일어나고 있다면, 곰팡이균 감염을 의심해 봐야 한다. 그뿐만 아니라 폐질환, 당뇨병, 갑상선 질환을 앓고 있는 환자도 종종 손톱이 노랗게 변할 수 있다.</div>
-					</li>
-					<div class="line mobile_display"></div>
-					<li>
-						<div class="circle_img" id="circle3"><img src="img/sub08/sub08_07.svg"></div>
-						<div class="item_title line01">손톱에 검은 세로줄이</div>
-						<div class="item_title line02">보인다면?</div>
-						<div class="item_text">손톱 밑에 숨은 흑색종을 의심해 볼 수 있다. 오랜 기간 여유를 두고 검은 세로줄을 관찰했을 때 검은 줄이 점점 커지거나 불규칙한 색을 띄면 피부과에 방문하길 추천한다. 흑색종은 멜라닌 세포 악성화로 인해 생긴 종양으로 신체 피부 어디에나 발생할 수 있으며 가장 많은 전이를 일으킨다. 흑색종이 내부 장기로 전이되면 매우 위험한 암이 될 수 있기 때문에 조기에 발견하고 완전히 제거하는 것이 좋다.</div>
-					</li>
-					<div class="line mobile_display"></div>
+
+				  <!-- </div> -->
+				<div class="col2 fadeBigInUp">
+				        <div class="men1_img"><img src="img/sub08/sub08_05.png"></div>
+					<!-- </li> -->
+					<!-- <li> -->
+						<div class="circle_img" ><img src="img/sub08/sub08_06.png"></div>
+						<!-- <div class="item_title line01">손톱 끝이 쉽게 갈라지고</div>
+						<div class="item_title line02">부서진다면?</div> -->
+						<div class="item_text">얼굴과 머리에서 땀이 많이 분비되는 현상을 두한증이라고 한다. 증상이 심하면 비가 오듯이 흐르는 땀 때문에 건강에 문제가 생긴 건 아닌지 오해를 받기도 한다. 식사할 땐 정수리에서 시작된 땀이 목을 타고 흐르기 때문에 두한증이 있는 사람은 매콤하거나 뜨거운 음식을 선택할 때 신중해야 한다. 여성의 경우 얼굴에서 흐르는 땀으로 인해 화장이 지워지는 불편함이 있을 수 있다.</div>
+					<!-- </li> -->
+					    <div class="line mobile_display"></div>
 				</div>
-				<div class="item3 fadeBigInUp">
-					<li class="left">
-						<div class="circle_img" id="circle4"><img src="img/sub08/sub08_08.svg"></div>
-						<div class="item_title line01">손톱 가운데가</div>
-						<div class="item_title line02">들어갔거나 솟았다면?</div>
-						<div class="item_text">철분이 부족할 경우 손톱의 가운데 부분이 움푹 들어갈 수 있다. 또한 여성의 경우 손톱 끝이 위로 뒤집어졌다면, 자궁 기능 이상을 의심해 볼 수 있다. 이럴 땐 철분 보충제나 철분이 많이 함유된 음식을 섭취하는 것이 좋다. 반면 손톱이 볼록 솟아오른 모양이라면 소화 기능에 이상이 생겼거나 호흡기 쪽 건강을 확인해야 한다.</div>
-					</li>
-					<div class="line mobile_display"></div>
-					<li>
-						<div class="circle_img" id="circle5"><img src="img/sub08/sub08_09.svg"></div>
-						<div class="item_title line01">손톱의 표면이 거칠고</div>
-						<div class="item_title line02">울퉁불퉁하면?</div>
-						<div class="item_text">손톱의 뿌리가 건조해져 나타나는 증상으로 일명 건선이다. 손톱 건선이 심해지면 손톱에 구멍이 생길 수 있고, 손톱이 빠질 가능성이 높아 일상생활의 불편함을 초래한다. 스트레스, 과로, 약물 오남용 등 건선의 주요 원인을 빠르게 파악하고 초기에 치료를 받는 것이 좋다. 특히 손톱 건선은 병이 발병한 부위만 치료해서는 완치가 되지 않고, 전체적으로 신체 면역력을 활성화해 피부 조직을 재생하는 과정으로 치료가 이루어지기 때문에 오랜 시간이 소요된다.</div>
-					</li>
+
+				<div class="col3 fadeBigInUp">
+						<div class="men2_img"><img src="img/sub08/sub08_07.png"></div>
+					<!-- </li> -->
+					<!-- <li> -->
+						<div class="circle_img" ><img src="img/sub08/sub08_08.png"></div>
+						<!-- <div class="item_title line01">손톱 끝이 쉽게 갈라지고</div>
+						<div class="item_title line02">부서진다면?</div> -->
+						<div class="item_text">유독 겨드랑이에 땀이 많이 난다면 액한증을 의심해 볼 수 있다. 통풍이 안 되는 블라우스를 입거나 꽉 끼는 상의를 입었을 때 겨드랑이가 축축하게 젖어 손을 들기 민망한 상황이 발생해 고민이 많은 부위이기도 하다. 겨드랑이는 아포크린샘*이라는 땀샘이 존재해서 땀 냄새를 심하게 유발하는 부위이다. 겨드랑이 땀을 피부의 정상 세균이 분해하는 과정에서 땀 냄새가 나는 것을 액취증이라고 하는데, 액한증이 지속된다면 액취증을 동반할 확률이 높아져 주의가 필요하다. </div>
+					<!-- </li> -->
+					    <div class="line mobile_display"></div>
 				</div>
+
+			 	</div>
+
 			</div>
 		</div>
 

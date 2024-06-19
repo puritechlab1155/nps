a62761a68d4cc0a1babf0a8aedf06e74931d0459
puritechlab <pub@puritechlab.com>
2024-06-19 17:37:06 +0900

    sub03 / sub06 angle_down edit

--- html/vol200/sub03.php
+++ html/vol200/sub03.php
@@ -19,7 +19,7 @@
 		<div class="section01 wow fadeBigInUp">
 			<div class="content">
 				<div class="title">M&Z일상수다</div>
-				<div class="angle-down"><i class="xi-angle-down-min"></i></div>
+				<div class="angle-down"><img src="../../img/common/ico_angle_down_wh.svg"></div>
 			</div>
 		</div>
 

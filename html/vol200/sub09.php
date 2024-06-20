3733cb1376da1460d7f744defc8b1d8415da66d7
puritechlab <pub@puritechlab.com>
2024-06-20 14:58:14 +0900

    sub09 14:58

--- html/vol200/sub09.php
+++ html/vol200/sub09.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="ko">
+
 <head>
 	<title>내 곁에 국민연금</title>
 
@@ -7,167 +8,139 @@
 	<?php include("../../inc/head.php"); ?>
 	<link rel="stylesheet" type="text/css" href="css/contents.css">
 </head>
+
 <body>
 
-<div id="wrap" class="sub09">
+	<div id="wrap" class="sub09">
 
-	<!-- header -->
-	<?php include("header.php"); ?>
-	<!-- //header -->
+		<!-- header -->
+		<?php include("header.php"); ?>
+		<!-- //header -->
 
-	<!-- contents -->
-	<section id="contents">
-		<div class="section01 wow fadeBigInUp">
-			<div class="content">
-				<div class="title">오늘의 레시피</div>
-				<div class="text">일러스트. 강영지</div>
-				<div class="angle-down"><img src="../../img/common/ico_angle_down_wh.svg"></div>
-			</div>
-		</div>
+		<!-- contents -->
+		<section id="contents">
+			<div class="section01 wow fadeBigInUp">
+				<div class="content">
+					<div class="title">오늘의 레시피</div>
+					<div class="text">일러스트. 강영지</div>
+					<div class="angle-down"><img src="../../img/common/ico_angle_down_wh.svg"></div>
+				</div><!-- //.content -->
+			</div><!-- //.section01 -->
 
-		<div class="section02 wow fadeBigInUp">
-			<div class="content">
-				<div class="content01 fadeBigInUp">
-					<div class="subtitle_top">입속에서 알알이 터지는 여름 간식 옥수수</div>
-					<div class="title pc_display">언제 먹어도 든든한<br>‘옥수수 크림 스프’ 레시피</div>
-					<div class="title mobile_display">언제 먹어도 든든한<br>‘옥수수 크림 스프’<br> 레시피</div>
-					<div class="subcopy">
-						때이른 무더위에 입맛은 뚝 떨어지고 기력은 도무지 되살아날 기미가 보이지 않는다. 이럴 때 입에 착착 감기는 제철 식재료 옥수수가 필요하다. 여름의 시작부터 끝까지 다양한 모습으로 변모하며 우리 밥상을 수놓는 옥수수를 소개한다.
-					</div>
-				</div><!-- //.content01 -->
-
-				<div class="content02 fadeBigInUp">
-					<div class="subtitle">멕시코의 옥수수 사랑</div>
-					<div class="img_box"><img src="img/sub09/sub09_02.jpg"></div>
-					<div class="text">옥수수를 이야기하는데 뜬금없이 멕시코가 나온다고 생각할 수 있지만, 옥수수와 멕시코는 떼려야 뗄 수 없는 관계다. 옥수수의 최초 탄생이 멕시코와 연결돼 있기 때문이다. <br>
-						멕시코인들은 7천 년 전부터 옥수수를 재배했다. 척박한 땅에서도 잘 자라는 옥수수이지만, 유난히 멕시코 토양과 기후가 옥수수 재배에 적합했기 때문에 멕시코 인디언들은 옥수수를 먹고 자랐다. 이는 인류 문명의 역사보다도 한참 전의 일이다. <br>
-						멕시코에서 마야문명이 시작된 후에도 옥수수는 주요 산물이었다. 매일 먹는 주식은 물론이고, ‘길에 떨어진 옥수수를 보고 줍지 않는 자는 지옥에서 벌을 받아야 한다’고 생각하는 마야인들도 허다했다. 어느 기록물에는 ‘마야인들은 예부터 옥수수가 신의 선물이자, 신이 옥수수를 빚어 인간을 창조했다고 믿으며 옥수수를 신성시 여겼다’라고 쓰였다. 멕시코인들의 옥수수 사랑은 민담에서도 볼 수 있다.
-						<div class="note">
-							<p>
-								<span>옥수수 도둑</span>
-								옛날 옛적 멕시코의 넓은 옥수수 밭을 가진 늙은 아버지가 있었다. 이 아버지에게는 아들 셋이 있었는데, 첫째는 게으름뱅이에 잠꾸러기, 둘째는 심술쟁이, 셋째는 성실한 아이였다. 어느 해 옥수수 밭에 도둑이 들었고, 아버지는 도둑을 잡는 아들에게 재산을 물려준다고 선언한다. <br>
-								첫째가 의기양양하게 옥수수 밭으로 가지만, 토르티야를 먹고 잠이 든다. 그때 개구리가 나타나 자신을 데려가 주면 도둑을 잡게 해 준다고 부탁한다. 기분 좋게 잠을 자던 첫째는 짜증나서 개구리를 우물에 던지고 다시 잠이 든다. 날이 밝았을 땐 이미 옥수수는 없어졌고, 도둑을 잡지 못한 첫째는 터덜터덜 집으로 돌아온다. <br>
-								둘째 역시 개구리를 우물에 던져 버리고 옥수수 밭 도둑을 기다린다. 그때 큰 새가 옥수수를 쪼아먹는 것을 보고 총을 쏘지만, 총알이 빗나간다. <br>
-								셋째는 개구리의 부탁을 들어주고 개구리와 함께 옥수수 밭으로 향한다. 개구리는 도중에 만난 우물 앞에서 셋째에게 소원을 빌어 보라 한다. <br>
-								“옥수수 밭 도둑을 잡게 해 주시고, 아름다운 아가씨를 배필로 얻게 해 주시고, 커다란 집에서 살게 해 주세요.” 셋째가 옥수수 밭을 지킬 때 큰 새가 다시 날아와 옥수수를 훔쳐 먹는다. 개구리는 새에게 총을 겨누는 셋째를 말리며 울기 시작한다. 그때 새가 아름다운 여인으로 변한다. 셋째는 여인에게 결혼을 요청하고 여인, 개구리와 집으로 돌아간다.
-							</p>
+			<div class="section02 wow fadeBigInUp">
+				<div class="content">
+					<div class="content01 fadeBigInUp">
+						<div class="subtitle_top">입속에서 알알이 터지는 여름 간식 옥수수</div>
+						<div class="title pc_display">언제 먹어도 든든한<br>‘옥수수 크림 스프’ 레시피</div>
+						<div class="title mobile_display">언제 먹어도 든든한<br>‘옥수수 크림 스프’<br> 레시피</div>
+						<div class="subcopy">
+							때이른 무더위에 입맛은 뚝 떨어지고 기력은 도무지 되살아날 기미가 보이지 않는다. 이럴 때 입에 착착 감기는 제철 식재료 옥수수가 필요하다. 여름의 시작부터 끝까지
+							다양한 모습으로 변모하며 우리 밥상을 수놓는 옥수수를 소개한다.
 						</div>
-						음식도 마찬가지다. 토르티야, 나초, 퀘사디야, 팝콘, 콘플레이크 모두 옥수수로 만든다. 옥수수가 지금까지 이들에게 든든한 식량으로 버팀목이 되어 주었기 때문에 옥수수는 멕시코인들에게 영원토록 성스러운 농작물로 여겨지고 있다.
+					</div><!-- //.content01 -->
+
+					<div class="content02 fadeBigInUp">
+						<div class="subtitle">멕시코의 옥수수 사랑</div>
+						<div class="img_box"><img src="img/sub09/sub09_02.jpg"></div>
+						<div class="text">옥수수를 이야기하는데 뜬금없이 멕시코가 나온다고 생각할 수 있지만, 옥수수와 멕시코는 떼려야 뗄 수 없는 관계다. 옥수수의 최초 탄생이
+							멕시코와 연결돼 있기 때문이다. <br>
+							멕시코인들은 7천 년 전부터 옥수수를 재배했다. 척박한 땅에서도 잘 자라는 옥수수이지만, 유난히 멕시코 토양과 기후가 옥수수 재배에 적합했기 때문에 멕시코 인디언들은
+							옥수수를 먹고 자랐다. 이는 인류 문명의 역사보다도 한참 전의 일이다. <br>
+							멕시코에서 마야문명이 시작된 후에도 옥수수는 주요 산물이었다. 매일 먹는 주식은 물론이고, ‘길에 떨어진 옥수수를 보고 줍지 않는 자는 지옥에서 벌을 받아야 한다’고
+							생각하는 마야인들도 허다했다. 어느 기록물에는 ‘마야인들은 예부터 옥수수가 신의 선물이자, 신이 옥수수를 빚어 인간을 창조했다고 믿으며 옥수수를 신성시 여겼다’라고
+							쓰였다. 멕시코인들의 옥수수 사랑은 민담에서도 볼 수 있다.
+							<div class="note">
+								<p>
+									<span>옥수수 도둑</span>
+									옛날 옛적 멕시코의 넓은 옥수수 밭을 가진 늙은 아버지가 있었다. 이 아버지에게는 아들 셋이 있었는데, 첫째는 게으름뱅이에 잠꾸러기, 둘째는 심술쟁이,
+									셋째는 성실한 아이였다. 어느 해 옥수수 밭에 도둑이 들었고, 아버지는 도둑을 잡는 아들에게 재산을 물려준다고 선언한다. <br>
+									첫째가 의기양양하게 옥수수 밭으로 가지만, 토르티야를 먹고 잠이 든다. 그때 개구리가 나타나 자신을 데려가 주면 도둑을 잡게 해 준다고 부탁한다. 기분
+									좋게 잠을 자던 첫째는 짜증나서 개구리를 우물에 던지고 다시 잠이 든다. 날이 밝았을 땐 이미 옥수수는 없어졌고, 도둑을 잡지 못한 첫째는 터덜터덜
+									집으로 돌아온다. <br>
+									둘째 역시 개구리를 우물에 던져 버리고 옥수수 밭 도둑을 기다린다. 그때 큰 새가 옥수수를 쪼아먹는 것을 보고 총을 쏘지만, 총알이 빗나간다. <br>
+									셋째는 개구리의 부탁을 들어주고 개구리와 함께 옥수수 밭으로 향한다. 개구리는 도중에 만난 우물 앞에서 셋째에게 소원을 빌어 보라 한다. <br>
+									“옥수수 밭 도둑을 잡게 해 주시고, 아름다운 아가씨를 배필로 얻게 해 주시고, 커다란 집에서 살게 해 주세요.” 셋째가 옥수수 밭을 지킬 때 큰 새가
+									다시 날아와 옥수수를 훔쳐 먹는다. 개구리는 새에게 총을 겨누는 셋째를 말리며 울기 시작한다. 그때 새가 아름다운 여인으로 변한다. 셋째는 여인에게
+									결혼을 요청하고 여인, 개구리와 집으로 돌아간다.
+								</p>
+							</div>
+							음식도 마찬가지다. 토르티야, 나초, 퀘사디야, 팝콘, 콘플레이크 모두 옥수수로 만든다. 옥수수가 지금까지 이들에게 든든한 식량으로 버팀목이 되어 주었기 때문에
+							옥수수는 멕시코인들에게 영원토록 성스러운 농작물로 여겨지고 있다.
+						</div>
+					</div><!-- //.content02 -->
+
+					<div class="content03 fadeBigInUp">
+						<div class="subtitle">한국으로 온 옥수수</div><br>
+						<div class="text">
+							수천 년간 멕시코에서 자생하던 옥수수는 1492년 콜럼버스에 의해 유럽으로 전파되고 16세기 인도와 중국을 거쳐 한국에도 모습을 나타냈다. 한국에서는 산간 지대에서
+							옥수수를 조금씩 생산하다가 1970년대 들어서 본격적으로 재배해 오늘날에 이르렀다. <br>
+							한국에서 만날 수 있는 옥수수는 찰옥수수, 단옥수수, 초당옥수수로 크게 세 가지이다. 찰옥수수는 옥수수 알이 노란색을 띄고 모양이 둥글다. 식감이 쫄깃쫄깃하고
+							씹을수록 고소해 찰옥수수라는 이름이 붙여졌으며 알이 작고 검은색을 띄는 것도 있다. 쪄먹었을 때 가장 쫀득쫀득한 식감을 가진다는 점도 특징이다. <br>
+							단옥수수는 옥수수 종류 중에서 당분 함량이 높은 품종으로 간식용이나 통조림용으로 종종 이용된다. 단옥수수는 다시 여러 품종으로 나뉘는 데 그중 초당옥수수가 가장 잘
+							알려졌다. 초당옥수수의 가장 큰 특징은 찰옥수수보다 당도가 두 배 이상 높다는 점이다. 또한 생으로 먹을 수 있을 만큼 식감이 부드러워 샐러드나 구이 등으로 다양하게
+							활용된다. <br>
+							만약 변비로 고생 중이라면 옥수수에 주목해 보자. 옥수수는 개당 약 10g의 수용성 식이섬유를 함유한다. 식이섬유가 모든 변비에 좋다고 할 수는 없지만, 일반적인
+							변비일 경우 다량의 식이섬유를 섭취하면 변의 양이 증가하고 변이 부드러워져 변비를 해소하는 데 도움을 준다. 또한 옥수수는 소화가 천천히 이루어져 포만감이 오래
+							지속되기 때문에 체중 조절에도 도움을 줄 수 있다. 단, 열량이 높기 때문에 옥수수만 과하게 섭취하는 건 추천하지 않는다. <br>
+							수천 년 전 태어나 인류의 주요 식량으로 자리잡은 옥수수. 다양한 종류와 훌륭한 효능으로 우리의 집 나간 입맛을 돌아오게 해줄 고마운 식재료다. 올여름에는 달콤한
+							옥수수 크림 스프를 식탁에 올려보는 건 어떨까?
+						</div>
+					</div><!-- //.content03 -->
+				</div><!-- //.content -->
+			</div><!-- //.section02 -->
+
+			<div class="section03 wow fadeBigInUp">
+				<div class="content">
+					<div class="title fadeBigInUp">
+						<img src="img/sub09/sub09_05.png">
 					</div>
-				</div><!-- //.content02 -->
-
-				<div class="content03 fadeBigInUp">
-					<div class="subitle">한국으로 온 옥수수</div>
-					<div class="text">
-						수천 년간 멕시코에서 자생하던 옥수수는 1492년 콜럼버스에 의해 유럽으로 전파되고 16세기 인도와 중국을 거쳐 한국에도 모습을 나타냈다. 한국에서는 산간 지대에서 옥수수를 조금씩 생산하다가 1970년대 들어서 본격적으로 재배해 오늘날에 이르렀다. <br>
-						한국에서 만날 수 있는 옥수수는 찰옥수수, 단옥수수, 초당옥수수로 크게 세 가지이다. 찰옥수수는 옥수수 알이 노란색을 띄고 모양이 둥글다. 식감이 쫄깃쫄깃하고 씹을수록 고소해 찰옥수수라는 이름이 붙여졌으며 알이 작고 검은색을 띄는 것도 있다. 쪄먹었을 때 가장 쫀득쫀득한 식감을 가진다는 점도 특징이다. <br>
-						단옥수수는 옥수수 종류 중에서 당분 함량이 높은 품종으로 간식용이나 통조림용으로 종종 이용된다. 단옥수수는 다시 여러 품종으로 나뉘는 데 그중 초당옥수수가 가장 잘 알려졌다. 초당옥수수의 가장 큰 특징은 찰옥수수보다 당도가 두 배 이상 높다는 점이다. 또한 생으로 먹을 수 있을 만큼 식감이 부드러워 샐러드나 구이 등으로 다양하게 활용된다. <br>
-						만약 변비로 고생 중이라면 옥수수에 주목해 보자. 옥수수는 개당 약 10g의 수용성 식이섬유를 함유한다. 식이섬유가 모든 변비에 좋다고 할 수는 없지만, 일반적인 변비일 경우 다량의 식이섬유를 섭취하면 변의 양이 증가하고 변이 부드러워져 변비를 해소하는 데 도움을 준다. 또한 옥수수는 소화가 천천히 이루어져 포만감이 오래 지속되기 때문에 체중 조절에도 도움을 줄 수 있다. 단, 열량이 높기 때문에 옥수수만 과하게 섭취하는 건 추천하지 않는다. <br>
-						수천 년 전 태어나 인류의 주요 식량으로 자리잡은 옥수수. 다양한 종류와 훌륭한 효능으로 우리의 집 나간 입맛을 돌아오게 해줄 고마운 식재료다. 올여름에는 달콤한 옥수수 크림 스프를 식탁에 올려보는 건 어떨까?
+					<div class="item1 fadeBigInUp">
+						<div class="item_title"><img src="img/sub09/sub09_07.svg"></div>
+						<div class="item_text">옥수수 2개, 버터 2조각, 우유 1컵+1/2컵, 생크림 1/2컵, 체다치즈 1장, 파마산 치즈가루 1스푼, 소금, 빠네 빵</div>
+						<div class="item_img"><img src="img/sub09/sub09_08.png"></div>
 					</div>
-				</div>
-				<div class="content04 fadeBigInUp">
-				<div class="ring" id="ring">
-    </div>
-
-<!--   <script>
-        // 현재 해상도에 따라 이미지 개수를 결정하고 해당 이미지를 표시하는 함수
-        function adjustImages() {
-            var width = window.innerWidth; // 현재 창의 너비
-            var count;
-
-            // 해상도에 따라 이미지 개수 결정
-            if (width >= 1000) {
-                count = 11;
-            } else if (width >= 940) {
-                count = 9;
-			} else if (width >= 820) {
-                count = 8;
-            } else if (width >= 760) {
-                count = 7;
-			} else if (width >= 680) {
-                count = 6;
-			} else if (width >= 580) {
-                count = 5;
-            } else if (width >= 480) {
-                count = 4;
-            } else {
-                count = 3;
-            }
-
-            var ring = document.getElementById("ring");
-            ring.innerHTML = ""; // 기존 내용 제거
-
-            // 이미지 생성 및 추가
-            for (var i = 0; i < count; i++) {
-                var img = document.createElement("img");
-                img.src = "img/sub09/sub09_03_1.svg"; // 이미지 소스 설정
-                img.alt = "Ring Image"; // 대체 텍스트 설정
-                ring.appendChild(img); // 이미지를 ring 요소에 추가
-            }
-        }
-
-        // 페이지 로드시 및 창 크기 변경시 adjustImages 함수 호출
-        window.onload = adjustImages;
-        window.onresize = adjustImages;
-    </script> -->
-					
+					<div class="item2 fadeBigInUp">
+						<div class="item_title"><img src="img/sub09/sub09_09.svg"></div>
+						<div class="item_text">옥수수는 씻어서 칼로 알을 긁어 낸다.</div>
+						<div class="item_img"><img src="img/sub09/sub09_10.png"></div>
+					</div>
+					<div class="item3 fadeBigInUp">
+						<div class="item_title"><img src="img/sub09/sub09_11.svg"></div>
+						<div class="item_text">냄비에 넣은 버터가 녹기 시작하면 옥수수 알을 넣고 볶는다.</div>
+						<div class="item_img"><img src="img/sub09/sub09_12.png"></div>
+					</div>
+					<div class="item4 fadeBigInUp">
+						<div class="item_title"><img src="img/sub09/sub09_13.svg"></div>
+						<div class="item_text">우유, 생크림, 소금을 넣고 한소끔 끓인다.</div>
+						<div class="item_img"><img src="img/sub09/sub09_14.png"></div>
+					</div>
+					<div class="item5 fadeBigInUp">
+						<div class="item_title"><img src="img/sub09/sub09_15.svg"></div>
+						<div class="item_text">체다치즈와 파마산 치즈가루를 넣고 고루 저어가며 마무리한다.</div>
+						<div class="item_img"><img src="img/sub09/sub09_16.png"></div>
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="section03 wow fadeBigInUp">
-			<div class="content">
-				<div class="title fadeBigInUp">
-					<img src="img/sub09/sub09_05.png">
-				</div>
-				<div class="item1 fadeBigInUp">
-					<div class="item_title"><img src="img/sub09/sub09_07.svg"></div>
-					<div class="item_text">미나리, 양파, 당근, 부침가루, 튀김가루, 소금, 식초, 올리브유 1큰술, 후추, 간장, 물</div>
-					<div class="item_img"><img src="img/sub09/sub09_08.png"></div>
-					<div class="item_img"><img src="img/sub09/sub09_17.png"></div>
-				</div>
-				<div class="item1 fadeBigInUp">
-					<div class="item_title"><img src="img/sub09/sub09_09.svg"></div>
-					<div class="item_text">미나리 잎 중 누렇게 뜨거나 검은 부분은 떼어내고 식초로 세척한다.<br>참고로 미나리는 줄기를 반쯤 잘라내고 연한 잎과 줄기 위주로 사용하는 게 좋다.</div>
-					<div class="item_img"><img src="img/sub09/sub09_10.png"></div>
-				</div>
-				<div class="item1 fadeBigInUp">
-					<div class="item_title"><img src="img/sub09/sub09_11.svg"></div>
-					<div class="item_text">잎은 잘게 다지고 줄기는 길쭉하게 썰어 모양을 내는 용도로 사용한다.<br>나머지 채소는 길게 채를 썬다.</div>
-					<div class="item_img"><img src="img/sub09/sub09_12.png"></div>
-				</div>
-				<div class="item1 fadeBigInUp">
-					<div class="item_title"><img src="img/sub09/sub09_13.svg"></div>
-					<div class="item_text">부침가루와 튀김가루를 3:2 비율로 계량하고 얼음물을 넣어 반죽 농도를 맞춘다.<br>미나리, 당근, 양파, 소금 한 꼬집을 넣고 걸쭉하게 섞는다.</div>
-					<div class="item_img"><img src="img/sub09/sub09_14.png"></div>
-				</div>
-				<div class="item1 fadeBigInUp">
-					<div class="item_title"><img src="img/sub09/sub09_15.svg"></div>
-					<div class="item_text">프라이팬에 식용유를 넉넉히 두르고 반죽을 한 숟가락 퍼 납작하게 부치면 완성!<br>중간 불에서 천천히 부쳐야 겉은 바삭하고 속은 촉촉한 미나리 전을 맛볼 수 있다.</div>
-					<div class="item_img"><img src="img/sub09/sub09_16.png"></div>
-				</div>
-			</div>
-		</div>
-
-		<!--div class="section04 wow fadeBigInUp">
+			<!--div class="section04 wow fadeBigInUp">
 			<div class="content">
 				<div class="item fadeBigInUp">
 					<img src="img/sub09/sub09_17.png">
 				</div>
 		</div-->
-	</section>
-	<!-- contents -->
+		</section>
+		<!-- contents -->
 
-	<!-- page ctrl -->
-	<div class="page_ctrl">
-		<?php include("page_ctrl.php"); ?>
+		<!-- page ctrl -->
+		<div class="page_ctrl">
+			<?php include("page_ctrl.php"); ?>
+		</div>
+		<!-- //page ctrl -->
 	</div>
-	<!-- //page ctrl -->
-</div>
 
 	<!-- footer -->
 	<?php include("footer.php"); ?>
 	<!-- //footer -->
 </body>
+
 </html>

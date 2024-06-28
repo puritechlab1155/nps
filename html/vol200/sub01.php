22c87924ec61c9422835116eeeb8e0a4f675a661
puritechlab <pub@puritechlab.com>
2024-06-28 15:13:20 +0900

    sub01 pc comp 15:13

--- html/vol200/sub01.php
+++ html/vol200/sub01.php
@@ -47,7 +47,7 @@
 			</div>
 		</div><!-- //.section03 -->
 
-		<div class="news01">
+		<div class="news news01 on">
 			<div class="section04 wow fadeBigInUp">
 				<div class="content">
 					<div class="text_box">
@@ -138,24 +138,24 @@
 			</div><!-- //.section05 -->
 		</div><!-- //.news01 -->
 
-		<div class="news02">
+		<div class="news news02">
 			<div class="section04 wow fadeBigInUp">
 				<div class="content">
 					<div class="text_box">
-						<div class="arrow"><img src=""></div>
+						<div class="arrow"><img src="img/sub01/sub01_03.svg"></div>
 						<div class="text">
-							어르신을 위한 낮은 이자의 편리한 대출, <br>
+							어르신을 위한 낮은 이자의 편리한 대출,<br>
 							노후긴급자금 개정 알림
 						</div>
 					</div>
-					<div class="img_box"><img src=""></div>
+					<div class="img_box"><img src="img/sub01/sub01_21.svg"></div>
 				</div>
 			</div><!-- //.section04 -->
 
 			<div class="section05 wow fadeBigInUp">
 				<div class="content">
 					<div class="anchor">
-						<div class="img"><img src="img/sub01/sub01_04.png"></div>
+						<div class="img"><img src="img/sub01/sub01_04.svg"></div>
 						<div class="question">
 							두 번째 뉴스입니다.<br>
 							국민연금공단은 국민연금 수급자를 대상으로 노후긴급자금 대부를 실시하고 있습니다. 국민연금공단에서 대출을 받을 수 있다는 사실이 낯설게 들리실 텐데요.<br><br>
@@ -168,12 +168,11 @@
 							<div class="text">
 								국민연금공단은 60세 이상 국민연금 수급자가 전월세 보증금, 의료비, 배우자 장제비, 재해복구비 용도의 긴급한 자금을 필요로 하는 경우에 일정 한도 내에서 낮은 금리로 대부금을 지급해 안정적인 노후생활을 지원하고 있습니다. 바로 국민연금의 이런 제도가 노후긴급자금, 다른 말로 실버론을 뜻합니다.
 							</div>
-							<div class="img"><img src="img/sub01/sub01_05.png"></div>
+							<div class="img"><img src="img/sub01/sub01_05.svg"></div>
 						</div>
-						<!-- <div class="graph_area"><img src="img/sub01/sub01_06.svg"></div> -->
 					</div>
-					<div class="anchor">
-						<div class="img"><img src="img/sub01/sub01_04.png"></div>
+					<div class="anchor alignCenter">
+						<div class="img"><img src="img/sub01/sub01_04.svg"></div>
 						<div class="question">
 							누가 언제 어떻게 국민연금의 노후긴급자금을 신청할 수 있는지 자세히 말씀해 주세요.
 						</div>
@@ -185,16 +184,16 @@
 								네. 국민연금 노후긴급자금의 대부 금액은 개인별 연간 연금 수령액의 2배(최고 1,000만 원 한도) 이내에서 실제 사용된 비용을 청구할 수 있는데요. <br><br>
 								예를 들어 매월 받는 연금액이 30만 원인 노령연금 수급자(본인 또는 배우자)가 의료비로 700만 원을 사용했으면 본인 연간 연금액 360만원의 2배인 720만 원을 한도로 실제 사용한 금액인 700만 원까지 대부할 수 있습니다.<br><br>아래의 표에서 자세히 살펴보겠습니다.
 							</div>
-							<div class="img"><img src="img/sub01/sub01_05.png"></div>
+							<div class="img"><img src="img/sub01/sub01_05.svg"></div>
 						</div>
 						<div class="graph_area">
 							<div class="textBold">국민연금 노후긴급자금(실버론)</div>
 							<div class="text_box">
-								<div class="text"><span>대상</span>60세 이상 국민연금 수급자 (연금급여 지급 정지 중인 자, 외국인 등은 제외)</div>
-								<div class="text"><span>대부 금액</span>연간 연금 수령액의 2배 이내에서 실소요 비용 (최고 1,000만 원 한도)</div>
-								<div class="text"><span>대부 용도</span>전월세 보증금, 의료비, 배우자 장제비, 재해복구비</div>
+								<div class="text"><img src="img/sub01/check.svg"><span>대상 </span>: 60세 이상 국민연금 수급자 (연금급여 지급 정지 중인 자, 외국인 등은 제외)</div>
+								<div class="text"><img src="img/sub01/check.svg"><span>대부 금액 </span>: 연간 연금 수령액의 2배 이내에서 실소요 비용 (최고 1,000만 원 한도)</div>
+								<div class="text"><img src="img/sub01/check.svg"><span>대부 용도 </span>: 전월세 보증금, 의료비, 배우자 장제비, 재해복구비</div>
 								<div class="text list">
-									<span>신청 기한</span>
+									<img src="img/sub01/check.svg"><span>신청 기한</span>
 									<ul>
 										<li>전월세 보증금 : (신규) 임차개시일 전·후 3개월 이내, (갱신) 갱신계약일로부터 3개월 이내</li>
 										<li>의료비 : 진료일로부터 6개월 이내</li>
@@ -206,7 +205,7 @@
 						</div>
 					</div>
 					<div class="anchor">
-						<div class="img"><img src="img/sub01/sub01_04.png"></div>
+						<div class="img"><img src="img/sub01/sub01_04.svg"></div>
 						<div class="question">
 							그렇군요, 60세 이상 국민연금 수급자 중 긴급한 사유 발생 시 본인 연간 연금 수령액의 2배 이내(최고 1,000만 원 한도)에서 대부 받을 수 있다. 잘 알겠습니다.<br><br>
 							그런데, 2024년 7월 1일을 기준으로 노후긴급자금 대부사업 지침이 개정되었다고 하는데요. 어떤 내용이 바뀌었나요?
@@ -219,11 +218,11 @@
 								네. 주요 개정 내용은 아래와 같습니다. 첫째, 대부신청 대상에 기초생활수급자를 포함하였습니다. 국민기초생활보장법(생계·의료·주거)에 의한 급여를 받는 기초생활수급자도 다른 제외 사유에 해당하지 않는다면 대부받을 수 있습니다.<br><br>
 								둘째, 대부 이자율 적용기준이 변경되었습니다. 기존 5년 만기 국고채권 수익률에 예금은행 가중평균 수신금리(신규취급액 기준) 기준을 추가하여 둘 중 낮은 금리를 대부 이자율로 채택합니다.
 							</div>
-							<div class="img"><img src="img/sub01/sub01_05.png"></div>
+							<div class="img"><img src="img/sub01/sub01_05.svg"></div>
 						</div>
 					</div>
-					<div class="anchor">
-						<div class="img"><img src="img/sub01/sub01_04.png"></div>
+					<div class="anchor alignCenter">
+						<div class="img"><img src="img/sub01/sub01_04.svg"></div>
 						<div class="question">
 							지금까지 노후긴급자금 대부에 관해 알아봤는데요. 노후긴급자금 신청 제외 대상, 신청 자격, 용도별 신청 기한 및 구비 서류 등을 국민연금 고객센터(국번없이 1355(유료)) 또는 가까운 국민연금공단 지사에 문의하시면 더욱 자세하게 알아보실 수 있습니다.
 						</div>
@@ -232,24 +231,24 @@
 			</div><!-- //.section05 -->
 		</div><!-- //.news02 -->
 
-		<div class="news03">
+		<div class="news news03">
 			<div class="section04 wow fadeBigInUp">
 				<div class="content">
 					<div class="text_box">
-						<div class="arrow"><img src=""></div>
+						<div class="arrow"><img src="img/sub01/sub01_03.svg"></div>
 						<div class="text">
-							국민연금 기금 운용수익률  <br>
+							국민연금 기금 운용수익률 <br>
 							작년에 이어 올해 1분기에도 호조세 이어가
 						</div>
 					</div>
-					<div class="img_box"><img src=""></div>
+					<div class="img_box"><img src="img/sub01/sub01_31.svg"></div>
 				</div>
 			</div><!-- //.section04 -->
 
 			<div class="section05 wow fadeBigInUp">
 				<div class="content">
 					<div class="anchor">
-						<div class="img"><img src="img/sub01/sub01_04.png"></div>
+						<div class="img"><img src="img/sub01/sub01_04.svg"></div>
 						<div class="question">
 							세 번째 뉴스입니다.<br>
 							지난해 사상 첫 기금적립금 1,000조 원을 돌파한 국민연금기금이 올해 1분기에도 양호한 성과를 이어가고 있습니다.<br><br>
@@ -264,15 +263,15 @@
 								국민연금공단 기금운용본부는 악화된 경제시장 속에서 고군분투하며 호조세를 유지해 나가고 있습니다. 연초 이후 미국 기술주 중심의 강세로 국내 및 해외주식 운용수익률이 양호했으나, 기준금리 인하 시점 지연 우려로 채권금리가 상승해 전체 수익률 상승 폭을 일부 제한했는데요.<br><br>
 								자산별 잠정수익률(금액가중수익률 기준)은 해외주식 13.45%, 국내주식 5.53%, 해외채권 4.48%, 국내채권 -0.01%, 대체투자 4.11%로 집계됐습니다.
 							</div>
-							<div class="img"><img src="img/sub01/sub01_05.png"></div>
+							<div class="img"><img src="img/sub01/sub01_05.svg"></div>
 						</div>
 						<div class="graph_area">
 							<div class="textBold">자산별 잠정수익률(금액가중수익률 기준)</div>
-							<div class="graph_area"><img src="" alt=""></div>
+							<div class="img_box"><img src="img/sub01/sub01_32.svg"></div>
 						</div>
 					</div>
-					<div class="anchor">
-						<div class="img"><img src="img/sub01/sub01_04.png"></div>
+					<div class="anchor alignCenter">
+						<div class="img"><img src="img/sub01/sub01_04.svg"></div>
 						<div class="question">
 							국내외 주식 시장에 관해서도 자세한 설명 부탁드립니다
 						</div>
@@ -284,11 +283,11 @@
 								네. 국내외 주식 시장에 관해 간략하게 말씀드리겠습니다. 국내주식(5.53%) 및 해외주식(13.45%)은 미국 인플레이션 경계감에도 인공지능(AI) 수요 기대 등 대형 기술주 중심으로 상승했으며, 해외주식은 원화 약세 효과가 더해져 두 자릿수의 운용수익률을 보였습니다.  <br><br>
 								참고로 국내 주식시장 상승률은 연초 대비 +3.44%이며, 글로벌 주식시장(달러 기준) 상승률은 연초 대비 +8.32% 입니다.
 							</div>
-							<div class="img"><img src="img/sub01/sub01_05.png"></div>
+							<div class="img"><img src="img/sub01/sub01_05.svg"></div>
 						</div>
 					</div>
-					<div class="anchor">
-						<div class="img"><img src="img/sub01/sub01_04.png"></div>
+					<div class="anchor alignCenter">
+						<div class="img"><img src="img/sub01/sub01_04.svg"></div>
 						<div class="question">
 							그렇다면, 국내외 채권 시장의 분위기는 어떤가요?
 						</div>
@@ -299,11 +298,11 @@
 							<div class="text">
 								네. 국내외 채권 시장은 여러 사정으로 약간 불안정했지만, 금세 호조세로 돌아섰는데요. 국내채권(-0.01%) 및 해외채권(4.48%)은 미국의 통화긴축 기조가 이어질 수 있다는 우려로 금리가 상승했으나, 해외채권은 원·달러 환율 상승으로 양호한 수익률을 보였습니다.
 							</div>
-							<div class="img"><img src="img/sub01/sub01_05.png"></div>
+							<div class="img"><img src="img/sub01/sub01_05.svg"></div>
 						</div>
 					</div>
 					<div class="anchor">
-						<div class="img"><img src="img/sub01/sub01_04.png"></div>
+						<div class="img"><img src="img/sub01/sub01_04.svg"></div>
 						<div class="question">
 							네. 국민연금 기금 운용수익률 중 국내외 주식은 대형 기술주와 원화 약세 효과가 더해져 상승했으며, 국내외 채권은 금리 상승에도 원·달러 환율 상승으로 수익률을 냈군요. <br><br>
 							국민연금 기금 운용수익률이 작년에 이어 끊임없이 호조세를 이어 나가는 가운데 국민연금공단의 김태현 이사장은 앞으로의 포부를 전해왔습니다.
@@ -314,16 +313,18 @@
 
 			<div class="section06 wow fadeBigInUp">
 				<div class="content">
-					<div class="text">
-						김태현 이사장은
-						<span>
-							“작년에 이어 올해도 양호한 성과를 이어가고 있습니다. <br>
-							국민의 소중한 노후자금을 운용하는 장기투자자로서 변동성이 큰 시장 환경에서도 <br>
-							안정적인 수익을 창출할 수 있도록 최선을 다하겠습니다”
-						</span>
-						라고 전하였습니다.
+					<div class="conts_box">
+						<div class="text">
+							김태현 이사장은
+							<span>
+								“작년에 이어 올해도 양호한 성과를 이어가고 있습니다. <br>
+								국민의 소중한 노후자금을 운용하는 장기투자자로서 변동성이 큰 시장 환경에서도 <br>
+								안정적인 수익을 창출할 수 있도록 최선을 다하겠습니다”
+							</span>
+							라고 전하였습니다.
+						</div>
+						<div class="img_box"><img src="img/sub01/sub01_33.svg"></div>
 					</div>
-					<div class="img_box"><img src="" alt=""></div>
 				</div>
 			</div><!-- //.section06 -->
 		</div><!-- //.news03 -->
@@ -334,6 +335,11 @@
 		$('.tabs div').click(function(){
 			$(this).addClass('on');
 			$(this).siblings().removeClass('on');
+
+			var idx = $(this).index();
+
+			$('.news').eq(idx).addClass('on');
+			$('.news').eq(idx).siblings().removeClass('on');
 		});
 	</script>
 

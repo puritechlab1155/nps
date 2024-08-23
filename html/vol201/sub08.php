9830963f6ef1c248d09ecc250a3363e5c5a455d2
puritechlab <pub@puritechlab.com>
2024-08-23 14:44:07 +0900

    edit 14:44

--- html/vol201/sub08.php
+++ html/vol201/sub08.php
@@ -52,73 +52,168 @@
 				햇볕을 많이 받을 때는 세로토닌과 도파민 분비가 활발해 행복감이 충만해지지만, 햇볕을 적게 받으면 세로토닌의 농도가 떨어지고 수면을 유도하는 멜라토닌이 분비돼 평소보다 쉽게 피로감을 느끼고 잠이 많아진다. 극야와 백야 현상으로 일조량의 변화폭이 극심한 북유럽 지역에서 우울증 환자가 많은 것도 같은 이유 때문이다.<br>
 				가을철 단순히 잠이 많아졌다며 ‘계절성 우울증’이라고 단정하는 건 금물이다. 우울장애 진단 기준에 따라 우울한 기분, 흥미나 즐거움의 감소, 체중 변화, 수면 이상, 정신운동 이상, 피로 등의 증상이 한 해의 일정 기간 사이에 규칙적으로 반복되는 양상을 보일 때 ‘계절성 우울증’이라고 진단할 수 있다.
 				</div>
-				<div class="item_box">
-					<ul class="item_list">
-						<li>
-							<div class="img_box"><img src="img/sub08/sub08_21.svg"></div>
-							<div class="text_box">
-								<span class="purple">세로토닌</span>
-								<p>
-									긍정적인 감정이나 행복감을 높이는 신경 전달 물질.
-								</p>
-							</div>
-						</li>
-						<li>
-							<div class="img_box"><img src="img/sub08/sub08_22.svg"></div>
-							<div class="text_box">
-								<span class="yellow">도파민</span>
-								<p>
-									쾌감 또는 즐거움을 조절하는 신경 전달 물질.
-								</p>
-							</div>
-						</li>
-						<li>
-							<div class="img_box"><img src="img/sub08/sub08_23.svg"></div>
-							<div class="text_box">
-								<span class="blue">멜라토닌</span>
-								<p>
-									수면과 생체리듬을 조절하는 호르몬.
-								</p>
-							</div>
-						</li>
-						<li>
-							<div class="img_box"><img src="img/sub08/sub08_24.svg"></div>
-							<div class="text_box">
-								<span class="green">엔도르핀</span>
-								<p>
-									통증을 완화하는 호르몬.
-								</p>
-							</div>
-						</li>
-						<li>
-							<div class="img_box img_pink"><img src="img/sub08/sub08_25.svg"></div>
-							<div class="text_box">
-								<span class="pink">옥시토신</span>
-								<p>
-									사랑과 신뢰라는 감정을 조절하는 호르몬.
-								</p>
-							</div>
-						</li>
-					</ul>
-					<div class="cap_box">
-						<p>
-							<span>신경 전달 물질 </span>: 내분비기관에서 생성돼 혈액을 통해 이동하며 내분비 시스템을 조절한다.<br>
-							<span>호르몬 </span>: 뉴런에서 생성돼 시냅스 간 화학적 신호를 전달하며 신경망을 조절한다.
-						</p>
+				<div class="pc_display">
+					<div class="item_box">
+						<ul class="item_list">
+							<li>
+								<div class="img_box"><img src="img/sub08/sub08_21.svg"></div>
+								<div class="text_box">
+									<span class="purple">세로토닌</span>
+									<p>
+										긍정적인 감정이나 행복감을 높이는 신경 전달 물질.
+									</p>
+								</div>
+							</li>
+							<li>
+								<div class="img_box"><img src="img/sub08/sub08_22.svg"></div>
+								<div class="text_box">
+									<span class="yellow">도파민</span>
+									<p>
+										쾌감 또는 즐거움을 조절하는 신경 전달 물질.
+									</p>
+								</div>
+							</li>
+							<li>
+								<div class="img_box"><img src="img/sub08/sub08_23.svg"></div>
+								<div class="text_box">
+									<span class="blue">멜라토닌</span>
+									<p>
+										수면과 생체리듬을 조절하는 호르몬.
+									</p>
+								</div>
+							</li>
+							<li>
+								<div class="img_box"><img src="img/sub08/sub08_24.svg"></div>
+								<div class="text_box">
+									<span class="green">엔도르핀</span>
+									<p>
+										통증을 완화하는 호르몬.
+									</p>
+								</div>
+							</li>
+							<li>
+								<div class="img_box img_pink"><img src="img/sub08/sub08_25.svg"></div>
+								<div class="text_box">
+									<span class="pink">옥시토신</span>
+									<p>
+										사랑과 신뢰라는 감정을 조절하는 호르몬.
+									</p>
+								</div>
+							</li>
+						</ul>
+						<div class="cap_box">
+							<p>
+								<span>신경 전달 물질 </span>: 내분비기관에서 생성돼 혈액을 통해 이동하며 내분비 시스템을 조절한다.<br>
+								<span>호르몬 </span>: 뉴런에서 생성돼 시냅스 간 화학적 신호를 전달하며 신경망을 조절한다.
+							</p>
+						</div>
 					</div>
-				</div>	
+				</div>
+				<div class="mobile_display">
+					<div class="item_box">
+						<ul class="item_list">
+							<li>
+								<div class="img_box"><img src="img/sub08/sub08_21.svg"></div>
+								<div class="text_box">
+									<span class="purple">세로토닌</span>
+									<p>
+										긍정적인 감정이나 행복감을 높이는 신경 전달 물질.
+									</p>
+								</div>
+							</li>
+							<li>
+								<div class="img_box"><img src="img/sub08/sub08_22.svg"></div>
+								<div class="text_box">
+									<span class="yellow">도파민</span>
+									<p>
+										쾌감 또는 즐거움을 조절하는 신경 전달 물질.
+									</p>
+								</div>
+							</li>
+						</ul>
+						<ul class="item_list">
+							<li>
+								<div class="img_box"><img src="img/sub08/sub08_23_mo.svg"></div>
+								<div class="text_box">
+									<span class="blue">멜라토닌</span>
+									<p>
+										수면과 생체리듬을 조절하는 호르몬.
+									</p>
+								</div>
+							</li>
+							<li>
+								<div class="img_box"><img src="img/sub08/sub08_24.svg"></div>
+								<div class="text_box">
+									<span class="green">엔도르핀</span>
+									<p>
+										통증을 완화하는 호르몬.
+									</p>
+								</div>
+							</li>
+						</ul>
+						<ul class="item_list">
+							<li>
+								<div class="img_box img_pink"><img src="img/sub08/sub08_25.svg"></div>
+								<div class="text_box">
+									<span class="pink">옥시토신</span>
+									<p>
+										사랑과 신뢰라는 감정을 조절하는 호르몬.
+									</p>
+								</div>
+							</li>
+							<li class="cap_box">
+								<p>
+									<span>신경 전달 물질 </span>: 내분비기관에서 생성돼 혈액을 통해 이동하며 내분비 시스템을 조절한다.<br>
+									<span>호르몬 </span>: 뉴런에서 생성돼 시냅스 간 화학적 신호를 전달하며 신경망을 조절한다.
+								</p>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div><!-- //.section04 -->
 
-		<div class="section05 wow fadeBigInUp"></div><!-- //section05 -->
+		<div class="section05 wow fadeBigInUp">
+			<div class="content">
+				<div class="subtitle">
+					유난히 울적한 기분이 든다면?<br>
+					계절성 우울증 예방법
+				</div>
+				<div class="type_explain">
+					<div class="types">
+						<div class="type1">
+							<div class="img_box"><img src="img/sub08/sub08_31.svg" class="hover-image"></div>
+							<span class="purple">햇빛 받고 기분 전환하기</span>
+							<p>
+								햇볕을 쬐는 것만으로도 금세 기분이 좋아질 수 있기 때문에 1~2시간 정도 외출 또는 운동을 추천한다. 운동은 스트레스 지수를 낮추고 에너지를 높여주며 정신적, 신체적 만족감을 준다. 또한 산소 섭취량을 늘려 주기 때문에 울적한 기분을 털어내는 데 도움을 준다. 야외 활동이 어렵다면, 실내조명을 높이고, 커튼을 열어 밝은 환경을 만드는 것도 좋다.
+							</p>
+						</div>
+						<div class="type2">
+							<div class="img_box"><img src="img/sub08/sub08_32.svg" class="hover-image"></div>
+							<span class="blue">규칙적인 생활하기</span>
+							<p>
+								신체 리듬을 유지하는 것도 계절성 우울증을 예방하는 데 좋다. 정해진 시간마다 잠자리에 들고, 일정한 시간에 식사하고, 운동을 습관화하는 등 규칙적인 생활은 몸과 마음의 건강을 유지하는 데 필수 요건이다. 잠자리에서 스마트폰을 사용하지 않는 것도 규칙적인 생활에 도움을 준다.
+							</p>
+						</div>
+						<div class="type3">
+							<div class="img_box"><img src="img/sub08/sub08_33.svg" class="hover-image"></div>
+							<span class="green">균형 잡힌 식사하기</span>
+							<p>
+								비타민D와 엽산을 섭취한다. 비타민D와 엽산은 체내 대사량이 부족할 경우 우울증을 일으키기도 한다. 또한, 하루 8잔의 수분을 섭취해 몸의 신진대사를 활성화하는 것도 추천한다. 수면을 방해하는 술, 카페인은 물론 설탕은 섭취하지 않는 편이 좋다.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!-- //section05 -->
 
 		<div class="section06 wow fadeBigInUp">
 			<div class="content">
 				<div class="pc_display">
 					<div class="title_box fadeBigInUp">
-						<img src="img/sub08/sub08-item.svg">
-						<div class="title">건강한 땀 배출을 위한 생활 수칙</div>
-						<img src="img/sub08/sub08-item.svg" class="title_item2">
+						<img src="img/sub08/sub08-item.png">
+						<div class="title">우울감을 해소해 주는 요가</div>
+						<img src="img/sub08/sub08-item.png" class="title_item2">
 					</div>
 				</div>
 				<div class="mobile_display">
@@ -127,37 +222,26 @@
 						<div class="title">건강한<br> 땀 배출을 위한<br> 생활 수칙</div>
 					</div>
 				</div>
-				<div class="copy-text">다한증 치료를 위해 교감신경을 절제하거나, 땀 분비를 막는 레이저 치료를 진행하기도 하지만 심하지 않은 다한증이라면 건강한 생활 습관 관리를 통해 증상을 완화할 수 있다.</div>
 				<div class="items">
 					<div class="item item1 fadeBigInUp">
-						<div class="item_img"><img src="img/sub08/sub08_31.svg" class="hover-image"></div>
-						<span>꾸준한 운동으로 땀 흘리기</span>
-						<p>운동은 몸 전체에 골고루 땀이 나면서 땀 분비 정상화를 돕는다. 꾸준한 운동으로 땀을 흘리고 가볍게 씻어내면 다한증 증상 완화에 큰 도움이 된다.</p>
+						<div class="item_img"><img src="img/sub08/sub08_41.svg" class="hover-image"></div>
+						<span>삼각 자세</span>
+						<p>두 발을 1m 간격으로 벌리고 팔은 어깨와 일직선이 되도록 들어 올린다. 숨을 들이켜며 척추를 길게 늘리고, 숨을 내쉬며 오른쪽으로 팔과 몸을 기울인다.</p>
 					</div>
 					<div class="item item2 fadeBigInUp">
-						<div class="item_img"><img src="img/sub08/sub08_32.svg" class="hover-image"></div>
-						<span>통풍이 잘되는 면 소재 옷 입기</span>
-						<p>레깅스나 스키니진 등 조이는 의복은 혈액 순환에 방해가 되므로 피하고, 흡수가 잘 되는 면 소재의 옷을 입는다.</p>
+						<div class="item_img"><img src="img/sub08/sub08_42.svg" class="hover-image"></div>
+						<span>낙타 자세</span>
+						<p>무릎을 꿇은 상태에서 엉덩이를 들어 올리고 양손을 발뒤꿈치에 짚는다. 골반과 가슴을 앞으로 밀면서 척추를 늘린다.</p>
 					</div>
 					<div class="item item3 fadeBigInUp">
-						<div class="item_img"><img src="img/sub08/sub08_33.svg" class="hover-image"></div>
-						<span>이온 음료 또는 물 마시기</span>
-						<p>카페인 성분은 혈액 순환을 촉진해 땀 분비를 증가시키므로 되도록 피하고, 카페인 대신 이온 음료나 물로 충분히 수분을 공급해 준다.</p>
+						<div class="item_img"><img src="img/sub08/sub08_43.svg" class="hover-image"></div>
+						<span>전사 자세</span>
+						<p>매트 위에 서서 등을 곧게 펴준 후 두 발을 벌리고 팔을 들어 올린다. 앞에 디딘 무릎이 90도가 되도록 굽히고 등을 곧게 편다.</p>
 					</div>
 					<div class="item item4 fadeBigInUp">
-						<div class="item_img"><img src="img/sub08/sub08_34.svg" class="hover-image"></div>
-						<span>샤워 후 충분히 몸을 말리기</span>
-						<p>샤워 후 몸을 충분히 말리지 않으면 겨드랑이, 사타구니 등 물기가 남아있는 부위에 곰팡이·세균이 번식해서 냄새가 나는 액취증을 유발할 수 있다. 샤워 후 깨끗한 수건으로 꼼꼼하게 몸을 말리는 것이 중요하다.</p>
-					</div>
-					<div class="item item5 fadeBigInUp">
-						<div class="item_img"><img src="img/sub08/sub08_35.svg" class="hover-image"></div>
-						<span>적정 체중을 유지하기</span>
-						<p>과체중과 비만은 땀 분비를 촉진한다. 급격한 체중 변화에 따른 호르몬 변화가 다한증을 유발하는 원인이 될 수 있으므로, 적정 체중을 유지하는 것이 좋다.</p>
-					</div>
-					<div class="item item6 fadeBigInUp">
-						<div class="item_img"><img src="img/sub08/sub08_36.svg" class="hover-image"></div>
-						<span>스트레스와 긴장 풀기</span>
-						<p>과도한 스트레스가 지속되고, 긴장 상태가 이어지면 교감신경이 흥분되어 땀 분비량이 증가할 수 있다. 그 때문에 즐거운 마음가짐, 충분한 수면, 적절한 취미 활동 등으로 스트레스와 긴장을 푸는 것이 다한증 개선에 도움된다.</p>
+						<div class="item_img"><img src="img/sub08/sub08_44.svg" class="hover-image"></div>
+						<span>머리 들어 올린 강아지 자세</span>
+						<p>매트 위에 배를 대고 지면을 누르면서 상체를 들어 올린다. 하늘을 바라본 후 10초간 자세를 유지한다.</p>
 					</div>
 				</div>
 			</div>

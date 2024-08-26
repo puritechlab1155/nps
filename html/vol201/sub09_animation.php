bd9e25a57b341bb6fd8c04ee4e1f4d07bd6e0580
puritechlab <pub@puritechlab.com>
2024-08-26 16:46:00 +0900

    edit 16:45

---
+++ html/vol201/sub09_animation.php
@@ -0,0 +1,215 @@
+<!doctype html>
+<html lang="ko">
+
+<head>
+	<title>내 곁에 국민연금</title>
+
+	<meta property="og:image" content="../../img/logosns/logosns_vol199.jpg">
+	<?php include("../../inc/head.php"); ?>
+	<link rel="stylesheet" type="text/css" href="css/contents.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    
+	
+</head>
+<body>
+	
+
+	<div id="wrap" class="sub09">
+
+		<!-- header -->
+		<?php include("header.php"); ?>
+		<!-- //header -->
+
+		<!-- contents -->
+		<section id="contents">
+			<div class="section01 wow fadeBigInUp">
+				<div class="content">
+					<div class="title">오늘의 레시피</div>
+					<div class="text">일러스트. 강영지</div>
+					<div class="angle-down"><img src="../../img/common/ico_angle_down_wh.svg"></div>
+				</div><!-- //.content -->
+			</div><!-- //.section01 -->
+
+			<div class="section02 wow fadeBigInUp">
+				<div class="content">
+					<div class="content01 fadeBigInUp">
+						<div class="subtitle_top">초가을의 정취를 품은<br class="mobile_display"> 송이버섯</div>
+						<div class="title pc_display">영양 듬뿍 담은 ‘송이버섯솥밥’ 레시피</div>
+						<div class="title mobile_display">영양 듬뿍 담은 <br>‘송이버섯솥밥’ 레시피</div>
+
+						<div class="subcopy">
+							여름의 막바지이자 가을의 시작, 9월. 지난 여름 무더위에 지친 심신을 달래 줄 생명이 자연에서 솟아오르고 있다. 향긋한 냄새를 풍기는 송이버섯이다.
+						</div>
+					</div><!-- //.content01 -->
+
+					<div class="content02 fadeBigInUp">
+						<div class="img_box"><img src="img/sub09/sub09_01.jpg"></div>
+						<div class="subtitle">식물과 공생하는 버섯</div>
+						<br class="pc_display">
+						<div class="text">
+							추석이 다가오면 마트 진열장에서 자연산 송이버섯을 만날 수 있다. 
+							맛깔스러운 자태에 이끌려 장바구니에 집어넣다가 1kg당 수십만 원을 호가하는 가격에 입이 떡 벌어진다. 
+							하지만 자연산 송이의 냄새를 한번 맡고 나서는 비싼 가격에 고개가 끄덕여진다. 
+							다른 버섯과 달리 강렬하고 향긋한 솔향이 몸속으로 퍼져 나가 마치 자연을 거니는 듯한 기분이 들게 한다. </br>
+							송이버섯을 포함한 모든 버섯은 생태계와 공생하는 고마운 곰팡이다. 
+							곰팡이는 식물이 남긴 영양분을 먹고 자라는데, 자연산 송이버섯의 경우 소나무 뿌리에 붙어 더부살이하며 소나무가 광합성으로 만든 탄수화물을 먹고 자란다. 
+							송이버섯의 포자가 소나무 영양분을 골라먹고 무럭무럭 자라는 과정에서 흙 속의 수분과 양분도 송이버섯 안으로 흡수된다. 
+							전문가들은 이때 버섯이 흡수한 무기물, 인, 질소 등 양분이 소나무에게 공급되어 뿌리를 보호하는 역할을 하는 것으로 추측하고 있다. </br>
+							송이버섯에게 번식의 기회를 부여한 소나무, 그런 소나무에게 수분과 양분으로 고마움을 돌려주는 송이버섯. 
+							이 둘의 독특한 공생관계가 명확히 밝혀지지 않았지만, 확실한 건 버섯은 식물들과 공생관계를 이루고, 나무의 생장을 돕는 등 생태계에서 중요한 역할을 담당하는 고마운 식재료이다.
+						</div>
+					</div><!-- //.content02 -->
+					
+					<div class="content03 fadeBigInUp">
+						<div class="subtitle">뛰어난 효능의 ‘금’송이</div>
+						<br class="pc_display">
+						<div class="text">
+							허준의 『동의보감』에는 ‘송이는 성분이 고르고 맛이 달며, 독은 없고 소나무 냄새를 포함해 향이 뛰어나다. 산중에 오래된 소나무 밑에서 소나무 기운을 받아 자라는 것으로 버섯 중 으뜸이다’라고 적혀 있다. 자연산 송이버섯이 오래전부터 버섯의 왕이었음을 알 수 있는 대목이다.<br>
+							송이버섯은 맛과 향뿐 아니라 효능도 뛰어난데, 혈중 콜레스테롤을 억제해 위장병, 당뇨병, 동맥경화, 고지혈증 등을 예방하는 데 도움을 준다. 한의학적으로는 위장의 기능을 북돋아 주고 소화력이 약한 사람에게 효과적이라고 알려졌다.<br>
+							송이버섯은 소나무, 미생물, 토양인자, 기상 환경 등 여러 조건이 맞아떨어져야만 생장할 수 있어 자연산으로만 채취할 수 있다. 우리나라에서는 경북 울진, 영주, 봉화 지방과 강원 강릉, 양양 지방에서 주로 나는데, 산림청의 허가를 받은 채취원만이 자연산 송이버섯을 채취할 수 있다. 본인 소유나 임차하지 않은 산에서 송이버섯을 채취하면 5년 이하 징역 또는 5천만 원 이하 벌금형에 처할 수 있으니 등산 중 송이버섯을 본다고 해도 함부로 채취해서는 안 된다.<br>
+							자연산 송이는 생으로 먹는 것이 일반적이지만, 더 풍부한 향을 느끼고 싶거나, 귀한 손님에게 대접하고자 한다면 송이버섯솥밭을 추천한다. 가을을 담은 송이버섯솥밥 레시피를 소개한다.
+						</div>
+					</div><!-- //.content03 -->
+
+					<div class="content04">
+						<div class="item_box">
+							<div class="item item01">
+								<div class="item_title">자연산 송이버섯의 조건</div>
+								<div class="img_box"><img src="img/sub09/sub09_02.jpg"></div>
+								<ul>
+									<li><img src="img/sub09/sub09_item_num1.svg"><span>송이버섯 특유의 솔향이 강하다.</span></li>
+									<li><img src="img/sub09/sub09_item_num2.svg"><span>갓이 완전히 펴지지 않았다.</span></li>
+									<li><img src="img/sub09/sub09_item_num3.svg"><span>줄기가 짧고 굵으며 무겁고 단단하다.</span></li>
+								</ul>
+							</div>
+							<div class="item item02">
+								<div class="item_title">자연산 송이버섯 보관 TIP</div>
+								<div class="img_box"><img src="img/sub09/sub09_03.jpg"></div>
+								<div class="pc_display">
+									<ul>
+										<li><img src="img/sub09/sub09_item2_num1.svg"><span>송이버섯은 씻지 말고 포자와 주변에 붙은 이물질을 턴다.</span></li>
+										<li><img src="img/sub09/sub09_item2_num2.svg"><span>신문지 또는 한지로 감싸 냉장실에 보관한다.</span></li>
+										<li><img src="img/sub09/sub09_item2_num3.svg"><span>랩으로 진공 포장 후 냉동하면 3개월간 보관할 수 있다.</span></li>
+									</ul>
+								</div>
+								<div class="mobile_display">
+									<ul>
+										<li><img src="img/sub09/sub09_item2_01.svg"></li>
+										<li><img src="img/sub09/sub09_item2_02.svg"></li>
+										<li><img src="img/sub09/sub09_item2_03.svg"></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div><!-- //content04 -->
+				</div><!-- //.content -->
+			</div><!-- //.section02 -->
+
+			<div class="section03 wow fadeBigInUp">
+				<div class="content">
+					
+					<div class="title wow fadeBigInUp">
+						<img src="img/sub09/sub09_05.png" class="scroll-image">
+					</div>
+					<script>
+						$(window).scroll(function(){
+							var st = $(this).scrollTop();
+							var evTop = $('.scroll-image').offset().top - 500;
+
+							if(st>=evTop){
+								$('.scroll-image').addClass('on');
+							}else{
+								$('.scroll-image').removeClass('on');
+							}
+						});
+					</script>
+
+					<ul>
+						<li class="item1 fadeBigInUp">
+							<div class="item_title"><img src="img/sub09/sub09_06.svg"></div>
+							<div class="item_text pc_display">
+								쌀 2컵, 송이버섯 2개, 은행 15알, 실파 5대, 다시마 육수 2컵, 간장 3큰술, 맛술 1큰술, <br>
+								고춧가루 1작은술, 다진 마늘 1/2작은술, 참기름 1큰술, 통깨 약간
+							</div>
+							<div class="item_text mobile_display">
+								쌀 2컵, 송이버섯 2개, 은행 15알, 실파 5대, <br>
+								다시마 육수 2컵, 간장 3큰술, 맛술 1큰술, <br>
+								고춧가루 1작은술, 다진 마늘 1/2작은술, <br>
+								참기름 1큰술, 통깨 약간
+							</div>
+							<div class="item_img"><img src="img/sub09/sub09_07.png"></div>
+						</li>
+
+						<li class="item2 fadeBigInUp">
+							<div class="item_title"><img src="img/sub09/sub09_08.svg"></div>
+							<div class="item_text">
+								쌀은 씻어 30분간 불린 후 체에 밭쳐 물기를 뺀다.<br>
+								<span>tip. 기호에 따라 찹쌀을 넣어도 좋다.</span>
+							</div>
+							<div class="item_img">
+								<img src="img/sub09/sub09_09-1.png" class="img1">
+								<img src="img/sub09/sub09_09-2.png" class="img2">
+							</div>
+						</li>
+
+						<li class="item3 fadeBigInUp">
+							<div class="item_title"><img src="img/sub09/sub09_10.svg"></div>
+							<div class="item_text">송이버섯은 결 대로 먹기 좋게 찢어 준비한다.</div>
+							<div class="item_img"><img src="img/sub09/sub09_11.png"></div>
+						</li>
+
+						<li class="item4 fadeBigInUp">
+							<div class="item_title"><img src="img/sub09/sub09_12.svg"></div>
+							<div class="item_text">냄비에 불린 쌀을 넣고 다시마 육수를 부은 후 버섯과 은행을 돌려 담고 끓인다.</div>
+							<div class="item_img"><img src="img/sub09/sub09_13.png"></div>
+						</li>
+
+						<li class="item5 fadeBigInUp">
+							<div class="item_title"><img src="img/sub09/sub09_14.svg"></div>
+							<div class="item_text">밥물이 끓어오르면 약불로 줄여 15~20분간 익힌 후, 불을 끄고 5분 정도 뜸을 들이면 완성!</div>
+							<div class="item_img"><img src="img/sub09/sub09_15.png"></div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+		<!-- contents -->
+
+		<!-- page ctrl -->
+		<div class="page_ctrl">
+			<?php include("page_ctrl.php"); ?>
+		</div>
+		<!-- //page ctrl -->
+	</div>
+	<script>
+	
+	// $(window).scroll(function(){
+	// 	var st = $(this).scrollTop();
+	// 	var evTop = $('.scroll-image').offset().top - 500;
+
+	// 	if(st>=evTop){
+	// 		$('.scroll-image' > img).addClass('show');
+	// 	}
+	// });
+	$(document).ready(function(){
+    $(".sub09 .section03 .content .title").scroll(function(){
+    $(".scroll-image").animate({left: '250px'});
+  });
+});
+
+$(document).ready(function(){
+  $(".scroll-image").srcoll(function(){
+    $("scroll-image").css("animation", "slideIn 2s ease-out forwards");
+  });
+});
+
+</script>
+
+
+	<!-- footer -->
+	<?php include("footer.php"); ?>
+	<!-- //footer -->
+</body>
+
+</html>

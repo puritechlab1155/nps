8fd505d194a81612c213700e5c5fbba885269c1b
puritechlab <pub@puritechlab.com>
2024-08-27 19:04:34 +0900

    edit sub06 mo 19:04

--- html/vol201/sub06.php
+++ html/vol201/sub06.php
@@ -31,7 +31,12 @@
 					영화 &lt;오베라는 남자&gt;로 읽는 <br>
 					국민연금 실업 크레딧
 				</div>
-				<div class="title mobile_display">한국 초저출생<br>현상에 던지는<br>따뜻한 메시지</div>
+				<div class="title mobile_display">
+					영화 <br>
+					&lt;오베라는 남자&gt;로 <br>
+					읽는 국민연금 <br>
+					실업 크레딧
+				</div>
 				<div class="subcopy">
 					스웨덴 작가 프레드릭 배크만의 장편 소설 &lt;오베라는 남자&gt;는 노년 남성의 이야기이다. 배크만의 블로그에서 탄생한 ‘오베’라는 캐릭터는 수많은 독자 덕분에 2012년 소설 &lt;오베라는 남자&gt;로 출판된다. 2015년엔 스웨덴에서 영화로 제작, 2023년엔 미국에서 &lt;오토라는 남자&gt;로 리메이크됐다. ‘오베’의 매력이 무엇이길래 전 세계 사람들이 ‘오베’에 열광하는 걸까?
 				</div>
@@ -68,9 +73,14 @@
 					하지만 오베의 삶은 평화롭지만은 않다. 삶의 절정마다 소중한 사람을 잃었다. 다행히도 도전적인 여자 소냐가 우울한 청년 오베를 구원했다. 열차에서 우연히 만난 소냐는 오베에게 새로운 희망이 됐다. 스페인 여행 중 사고로 뱃속 아기와 두 다리를 잃지만, 소냐는 도전을 멈추지 않았다. 특수학급의 교사가 되어 어긋나는 청소년들을 바로잡는다. 그랬던 아내 소냐는 지금 없다. 옷걸이에 걸려있는 그녀의 옷과 탁상 위 그녀의 사진에서 그녀의 흔적을 찾을 수 있을 뿐이다.<br>
 					아내의 비석 앞에선 오베는 아내를 따라가겠다고 결심한다. 남은 것은 ‘어떻게 세상과 이별하느냐’이다. 천장에 끈을 달아보고, 차량 안을 매연으로 채워보지만 그때마다 들이닥치는 이웃들 때문에 계획은 번번이 무산된다. 마침내 오베는 짜증을 낸다. “죽기가 살기보다 더 힘들어!”
 				</div>
-				<div class="line_text">
+				<div class="line_text pc_display">
 					<div class="sub_text fadeBigInUp">삶의 벼랑에 선 무뚝뚝한 이 남자,</div>
 					<div class="sub_text fadeBigInUp">그의 성질을 살살 긁는 이웃과의 마지막은 어떤 모습이 될까?</div>
+				</div>
+				<div class="line_text mobile_display">
+					<div class="sub_text fadeBigInUp">삶의 벼랑에 선 무뚝뚝한 이 남자,</div>
+					<div class="sub_text fadeBigInUp">그의 성질을 살살 긁는 이웃과의 마지막은</div>
+					<div class="sub_text fadeBigInUp">어떤 모습이 될까?</div>
 				</div>
 				<h4>노령연금 수급 기회를 확대하는 실업 크레딧</h4>
 				<br class="pc_display">
@@ -87,72 +97,151 @@
 
 		<div class="section05 wow fadeBigInUp">
 			<div class="content">
-				<div class="cont_box">
-					<div class="item item1">
-						<div class="text_box">
-							<div class="box_title">
-								한눈에 보는
-								<span>실업 크레딧</span>
+				<div class="pc_display">
+					<div class="cont_box">
+						<div class="item item1">
+							<div class="text_box">
+								<div class="box_title">
+									한눈에 보는
+									<span>실업 크레딧</span>
+								</div>
+								<div class="item_q">
+									<img src="img/sub06/sub06_check.svg">
+									<span>실업 크레딧이란?</span>
+								</div>
+								<p>
+									만 18세 이상 60세 미만의 ‘구직급여 수급자’가 국민연금 가입기간 추가를 희망하는 경우, 1인 생애 최대 12개월까지 국가가 국민연금 보험료의 75%를 지원하고 그 기간만큼 국민연금 가입기간을 추가로 인정해 국민연금 수급 기회를 확대하는 제도이다.
+								</p>
 							</div>
-							<div class="item_q">
-								<img src="img/sub06/sub06_check.svg">
-								<span>실업 크레딧이란?</span>
+							<div class="img_box">
+								<img src="img/sub06/sub06_03.svg">
 							</div>
-							<p>
-								만 18세 이상 60세 미만의 ‘구직급여 수급자’가 국민연금 가입기간 추가를 희망하는 경우, 1인 생애 최대 12개월까지 국가가 국민연금 보험료의 75%를 지원하고 그 기간만큼 국민연금 가입기간을 추가로 인정해 국민연금 수급 기회를 확대하는 제도이다.
-							</p>
 						</div>
-						<div class="img_box">
-							<img src="img/sub06/sub06_03.svg">
-						</div>
-					</div>
 
-					<div class="item item2">
-						<div class="text_box">
-							<div class="item_q">
-								<img src="img/sub06/sub06_check.svg">
-								<span>지원 대상</span>
-							</div>
-							<p>
-								구직급여 수급자 중 국민연금 보험료를 1개월 이상 납부한 이력이 있는 가입자(였던자 포함)가 재산 및 소득 기준을 모두 충족한 경우 지원 가능하다.
-							</p>
-							<ul>
-								<li><span>재산 기준</span>재산세 과세표준의 합 6억 원 이하</li>
-								<li><span>소득 기준</span>종합소득(사업·근로소득 제외) 연 1,680만 원 이하</li>
-							</ul>
-						</div>
-					</div>
-
-					<div class="item item3">
-						<div class="text_box">
-							<div class="item_q">
-								<img src="img/sub06/sub06_check.svg">
-								<span>보험료 산정 기준</span>
-							</div>
-							<p>
-								실업크레딧 보험료는 인정소득을 기준으로 산정하여 납부한다. 인정소득은 실직하기 직전에 받았던 3개월간 평균소득의 50%에 해당하는 금액으로, 최대 70만 원을 초과할 수 없다.
-							</p>
-							<div class="list_box">
-								<div class="list_title"><span>예시</span>실직 전 3개월간 평균소득이 100만 원일 경우</div>
+						<div class="item item2">
+							<div class="text_box">
+								<div class="item_q">
+									<img src="img/sub06/sub06_check.svg">
+									<span>지원 대상</span>
+								</div>
+								<p>
+									구직급여 수급자 중 국민연금 보험료를 1개월 이상 납부한 이력이 있는 가입자(였던자 포함)가 재산 및 소득 기준을 모두 충족한 경우 지원 가능하다.
+								</p>
 								<ul>
-									<li><img src="img/sub06/sub06_check2.svg">보험료 부과 기준: 100만 원(3개월간 평균소득)의 50% = 500,000원(인정소득)</li>
-									<li><img src="img/sub06/sub06_check2.svg">월 보험료: 50만 원(인정소득) X 9%(국민연금 보험료율) = 45,000원</li>
-									<li><img src="img/sub06/sub06_check2.svg">본인 부담분(실제 납부할 보험료): 월 보험료의 25% = 11,250원</li>
-									<li><img src="img/sub06/sub06_check2.svg">국가 지원분: 월 보험료의 75% = 33,750원</li>
+									<li><span>재산 기준</span>재산세 과세표준의 합 6억 원 이하</li>
+									<li><span>소득 기준</span>종합소득(사업·근로소득 제외) 연 1,680만 원 이하</li>
 								</ul>
 							</div>
 						</div>
-					</div>
 
-					<div class="item item4">
-						<div class="text_box">
-							<div class="item_q">
-								<img src="img/sub06/sub06_check.svg">
-								<span>신청 방법</span>
+						<div class="item item3">
+							<div class="text_box">
+								<div class="item_q">
+									<img src="img/sub06/sub06_check.svg">
+									<span>보험료 산정 기준</span>
+								</div>
+								<p>
+									실업크레딧 보험료는 인정소득을 기준으로 산정하여 납부한다. 인정소득은 실직하기 직전에 받았던 3개월간 평균소득의 50%에 해당하는 금액으로, 최대 70만 원을 초과할 수 없다.
+								</p>
+								<div class="list_box">
+									<div class="list_title"><span>예시</span>실직 전 3개월간 평균소득이 100만 원일 경우</div>
+									<ul>
+										<li><img src="img/sub06/sub06_check2.svg">보험료 부과 기준: 100만 원(3개월간 평균소득)의 50% = 500,000원(인정소득)</li>
+										<li><img src="img/sub06/sub06_check2.svg">월 보험료: 50만 원(인정소득) X 9%(국민연금 보험료율) = 45,000원</li>
+										<li><img src="img/sub06/sub06_check2.svg">본인 부담분(실제 납부할 보험료): 월 보험료의 25% = 11,250원</li>
+										<li><img src="img/sub06/sub06_check2.svg">국가 지원분: 월 보험료의 75% = 33,750원</li>
+									</ul>
+								</div>
 							</div>
-							<p>
-								구직급여 신청 시 고용노동부 고용복지플러스센터에서 신청 가능하다. 구직급여를 받는 중이라면 구직급여 종료일이 속하는 달의 다음 달 15일까지 국민연금공단 지사 방문 또는 홈페이지를 통해 신청할 수 있다.
-							</p>
+						</div>
+
+						<div class="item item4">
+							<div class="text_box">
+								<div class="item_q">
+									<img src="img/sub06/sub06_check.svg">
+									<span>신청 방법</span>
+								</div>
+								<p>
+									구직급여 신청 시 고용노동부 고용복지플러스센터에서 신청 가능하다. 구직급여를 받는 중이라면 구직급여 종료일이 속하는 달의 다음 달 15일까지 국민연금공단 지사 방문 또는 홈페이지를 통해 신청할 수 있다.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="mobile_display">
+					<div class="cont_box">
+						<div class="text_box">
+							<div class="box_title">
+								한눈에 보는
+								<span>실업 <br class="mobile_display">크레딧</span>
+							</div>
+							<div class="img_box">
+								<img src="img/sub06/sub06_03_mo.svg">
+							</div>
+						</div>
+
+						<div class="item_area">
+							<div class="item item0">
+								<div class="text_box">
+									<div class="item_q">
+										<img src="img/sub06/sub06_check.svg">
+										<span>실업 크레딧이란?</span>
+									</div>
+									<p>
+										만 18세 이상 60세 미만의 ‘구직급여 수급자’가 국민연금 가입기간 추가를 희망하는 경우, 1인 생애 최대 12개월까지 국가가 국민연금 보험료의 75%를 지원하고 그 기간만큼 국민연금 가입기간을 추가로 인정해 국민연금 수급 기회를 확대하는 제도이다.
+									</p>
+								</div>
+							</div>
+
+							<div class="item item2">
+								<div class="text_box">
+									<div class="item_q">
+										<img src="img/sub06/sub06_check.svg">
+										<span>지원 대상</span>
+									</div>
+									<p>
+										구직급여 수급자 중 국민연금 보험료를 1개월 이상 납부한 이력이 있는 가입자(였던자 포함)가 재산 및 소득 기준을 모두 충족한 경우 지원 가능하다.
+									</p>
+									<ul>
+										<li><span>재산 기준</span><em>재산세 과세표준의 합 6억 원 이하</em></li>
+										<li><span>소득 기준</span><em>종합소득(사업·근로소득 제외) <br>연 1,680만 원 이하</em></li>
+									</ul>
+								</div>
+							</div>
+
+							<div class="item item3">
+								<div class="text_box">
+									<div class="item_q">
+										<img src="img/sub06/sub06_check.svg">
+										<span>보험료 산정 기준</span>
+									</div>
+									<p>
+										실업크레딧 보험료는 인정소득을 기준으로 산정하여 납부한다. 인정소득은 실직하기 직전에 받았던 3개월간 평균소득의 50%에 해당하는 금액으로, 최대 70만 원을 초과할 수 없다.
+									</p>
+									<div class="list_box">
+										<div class="list_title"><span>예시</span><em>실직 전 3개월간 평균소득이 100만 원일 경우</em></div>
+										<ul>
+											<li><img src="img/sub06/sub06_check2.svg"><em>보험료 부과 기준: 100만 원(3개월간 평균소득)의 50% = 500,000원(인정소득)</em></li>
+											<li><img src="img/sub06/sub06_check2.svg"><em>월 보험료: 50만 원(인정소득) X 9%(국민연금 보험료율) = 45,000원</em></li>
+											<li><img src="img/sub06/sub06_check2.svg"><em>본인 부담분(실제 납부할 보험료): <br>월 보험료의 25% = 11,250원</em></li>
+											<li><img src="img/sub06/sub06_check2.svg"><em>국가 지원분: <br>월 보험료의 75% = 33,750원</em></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+
+							<div class="item item4">
+								<div class="text_box">
+									<div class="item_q">
+										<img src="img/sub06/sub06_check.svg">
+										<span>신청 방법</span>
+									</div>
+									<p>
+										구직급여 신청 시 고용노동부 고용복지플러스센터에서 신청 가능하다. 구직급여를 받는 중이라면 구직급여 종료일이 속하는 달의 다음 달 15일까지 국민연금공단 지사 방문 또는 홈페이지를 통해 신청할 수 있다.
+									</p>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>

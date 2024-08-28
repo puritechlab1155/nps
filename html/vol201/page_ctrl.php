36d8cedc1bd7b2617db39eb59ea4671fa391c530
puritech <pub@puritechlab.com>
2024-08-28 17:20:02 +0900

    정리

--- html/vol201/page_ctrl.php
+++ html/vol201/page_ctrl.php
@@ -11,15 +11,25 @@
 <!---a href="https://www.nps.or.kr/jsppage/cyber_pr/subscribe/intro.jsp" target="_blank"  class="subscribe">구독</a--><!--#subscribe  btn_layer-->
 <a href="https://www.nps.or.kr/jsppage/cyber_pr/webzine1.jsp" target="_blank"  class="last_vol">지난 호</a>
 <!---a href="#top" class="top">TOP</a--->
+<!-- <?php
+// 이전 페이지 및 다음 페이지가 설정되었는지 확인하고 버튼 생성
+// echo "이전 페이지: $prev_page, 다음 페이지: $next_page<br>";
+
+// if ($prev_page !== "" && $prev_page !== "/html/vol201/index.php") {
+//     echo '<a class="prev_page" href="' . $prev_page . '"><i class="xi-angle-left-thin"></i></a>';
+// }
+// if ($next_page !== "" && $next_page !== "/html/vol201/index.php") {
+//     echo '<a class="next_page" href="' . $next_page . '"><i class="xi-angle-right-thin"></i></a>';
+// }
+?> -->
+
 <?php
 // 이전 페이지 및 다음 페이지가 설정되었는지 확인하고 버튼 생성
-echo "이전 페이지: $prev_page, 다음 페이지: $next_page<br>";
-
-if ($prev_page !== "" && $prev_page !== "/html/vol201/index.php") {
-    echo '<a class="prev_page" href="' . $prev_page . '"><i class="xi-angle-left-thin"></i></a>';
+if ($prev_page !== "" && $prev_page !== "/html/vol199/index.php") {
+    echo '<a class="prev_page" href="' . $prev_page . '"><img src="img/left.png"></a>';
 }
-if ($next_page !== "" && $next_page !== "/html/vol201/index.php") {
-    echo '<a class="next_page" href="' . $next_page . '"><i class="xi-angle-right-thin"></i></a>';
+if ($next_page !== "" && $next_page !== "/html/vol199/index.php") {
+    echo '<a class="next_page" href="' . $next_page . '"><img src="img/right.png"></a>';
 }
 ?>
 

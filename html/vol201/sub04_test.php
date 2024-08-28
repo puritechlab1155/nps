56b02e122c78c1719dc7010a0601f9c21c25de98
puritechlab <pub@puritechlab.com>
2024-08-28 15:19:38 +0900

    edit sub04 15:19

--- html/vol201/sub04_test.php
+++ html/vol201/sub04_test.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" type="text/css" href="css/contents.css">
 </head>
 <body>
-<div id="wrap" class="sub04">
+<div id="wrap" class="sub04 sub04_test">
 
 	<!-- header  -->
 	<?php include("header.php"); ?>

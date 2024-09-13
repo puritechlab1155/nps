03299635f3576b06c7ca2b330abe6a4b91e0d7da
puritechlab <pub@puritechlab.com>
2024-09-13 12:26:42 +0900

    newsletter 12:26

--- html/vol201/newsletter.php
+++ html/vol201/newsletter.php
@@ -1,29 +1,29 @@
 <!doctype html>
 <html lang="ko">
 <head>
-	<title>내 곁에 국민연금 NEWSLETTER 2024.04.23</title>
+	<title>내 곁에 국민연금 NEWSLETTER 2024.09.13</title>
     <META http-equiv=Content-Type content="text/html; charset=utf-8">
     </head>
 <body style="text-align: center;">
-<img src="https://www.npswebzine.kr/html/vol200/img/newsletter/nps_newsletter_200.jpg" usemap="#Map">
+<img src="https://www.npswebzine.kr/html/vol201/img/newsletter/nps_newsletter_201.jpg" usemap="#Map">
     <map name="Map">
-      <area shape="rect" coords="33,36,173,67" href="https://npswebzine.kr/html/vol200/index.php" target="_blank" title="내 곁에 국민연금 NEWSLETTER" alt="내 곁에 국민연금 NEWSLETTER"> 
-      <area shape="rect" coords="297,19,506,85" href="https://npswebzine.kr/html/vol200/index.php" target="_blank" title="내 곁에 국민연금 NEWSLETTER" alt="내 곁에 국민연금 NEWSLETTER"> 
-      <area shape="circle" coords="744,52,17" href="https://npswebzine.kr/html/vol200/index.php" target="_blank" title="내 곁에 국민연금 NEWSLETTER" alt="내 곁에 국민연금 NEWSLETTER"> 
-      <area shape="rect" coords="40,260,272,547" href="https://npswebzine.kr/html/vol200/sub01.php" target="_blank" title="NPS 뉴스 국민연금 주요 뉴스" alt="NPS 뉴스 국민연금 주요 뉴스">
-      <area shape="rect" coords="286,262,516,569" href="https://npswebzine.kr/html/vol200/sub02.php" target="_blank" title="NPS 칼럼 유족연금 수급권 제한의 오해와 진실" alt="NPS 칼럼 유족연금 수급권 제한의 오해와 진실">
-      <area shape="rect" coords="531,260,759,569" href="https://npswebzine.kr/html/vol200/sub03.php" target="_blank" title="M&Z일상수다 여름휴가를 보내는 다양한 방법!" alt="M&Z일상수다 여름휴가를 보내는 다양한 방법!">
-      <area shape="rect" coords="40,596,270,906" href="https://npswebzine.kr/html/vol200/sub04.php" target="_blank" title="알잘딱깔센! 세이브 머니 태아부터 부모까지 모두를 위한 8개 출생 지원 정책" alt="알잘딱깔센! 세이브 머니 태아부터 부모까지 모두를 위한 8개 출생 지원 정책">
-      <area shape="rect" coords="285,596,515,905" href="https://npswebzine.kr/html/vol200/sub05.php" target="_blank" title="모두의 여행 전북 고창에서 우리 '함께' 여름을 즐기다" alt="모두의 여행 전북 고창에서 우리 '함께' 여름을 즐기다">
-      <area shape="rect" coords="531,596,760,906" href="https://npswebzine.kr/html/vol200/sub06.php" target="_blank" title="연금엔씨네마 영화 <두 인생을 살아봐>로 읽는 출산 크레딧 제도" alt="연금엔씨네마 영화 <두 인생을 살아봐>로 읽는 출산 크레딧 제도">
-      <area shape="rect" coords="41,932,270,1242" href="https://npswebzine.kr/html/vol200/sub07.php" target="_blank" title="키워드로 보는 세상 편안한 노후를 위한 AI 이용 가이드" alt="키워드로 보는 세상 편안한 노후를 위한 AI 이용 가이드">
-      <area shape="rect" coords="285,932,515,1219" href="https://npswebzine.kr/html/vol200/sub08.php" target="_blank" title="헬스 앤 라이프 여름철 다한증이 고민된다면?" alt="헬스 앤 라이프 여름철 다한증이 고민된다면?">
-      <area shape="rect" coords="531,932,760,1242" href="https://npswebzine.kr/html/vol200/sub09.php" target="_blank" title="오늘의 레시피 입속에서 알알이 터지는 여름 간식 옥수수" alt="오늘의 레시피 입속에서 알알이 터지는 여름 간식 옥수수">
-      <area shape="rect" coords="41,1398,759,1577" href="https://npswebzine.kr/html/vol200/sub10.php" target="_blank" title="독자목소리 독자 여려분의 의견을 들려주세요." alt="독자목소리 독자 여려분의 의견을 들려주세요.">
-      <area shape="rect" coords="39,1603,759,1784" href="https://npswebzine.kr/html/vol200/sub11.php" target="_blank" title="200호 기념 이벤트" alt="200호 기념 이벤트">
-      <area shape="rect" coords="40,1809,759,1988" href="https://npswebzine.kr/html/vol200/sub12.php" target="_blank" title="빈칸퀴즈 웹진을 읽고 빈칸을 맞혀주세요!" alt="빈칸퀴즈 웹진을 읽고 빈칸을 맞혀주세요!">
-      <area shape="rect" coords="41,2014,759,2193" href="https://npswebzine.kr/html/vol200/sub13.php" target="_blank" title="구독자 뽑기 이벤트" alt="구독자 뽑기 이벤트">
-      <area shape="rect" coords="40,2218,761,2397" href="https://npswebzine.kr/html/vol200/sub14.php" target="_blank" title="더 알고 싶은 연금제도를 뽑아라!" alt="더 알고 싶은 연금제도를 뽑아라!">
+      <area shape="rect" coords="33,36,173,67" href="https://npswebzine.kr/html/vol201/index.php" target="_blank" title="내 곁에 국민연금 NEWSLETTER" alt="내 곁에 국민연금 NEWSLETTER"> 
+      <area shape="rect" coords="297,19,506,85" href="https://npswebzine.kr/html/vol201/index.php" target="_blank" title="내 곁에 국민연금 NEWSLETTER" alt="내 곁에 국민연금 NEWSLETTER"> 
+      <area shape="circle" coords="744,52,17" href="https://npswebzine.kr/html/vol201/index.php" target="_blank" title="내 곁에 국민연금 NEWSLETTER" alt="내 곁에 국민연금 NEWSLETTER"> 
+      <area shape="rect" coords="41,261,271,545" href="https://npswebzine.kr/html/vol201/sub01.php" target="_blank" title="NPS 뉴스. 국민연금 주요 뉴스" alt="NPS 뉴스 국민연금 주요 뉴스">
+      <area shape="rect" coords="284,260,515,568" href="https://npswebzine.kr/html/vol201/sub02.php" target="_blank" title="NPS 칼럼. 노후 소득 보장의 중추, 지속가능하고 튼튼한 버팀목 국민연금" alt="NPS 칼럼. 노후 소득 보장의 중추, 지속가능하고 튼튼한 버팀목 국민연금">
+      <area shape="rect" coords="530,260,759,545" href="https://npswebzine.kr/html/vol201/sub03.php" target="_blank" title="M&Z일상수다. 추석을 준비하는 다양한 방법!" alt="M&Z일상수다. 추석을 준비하는 다양한 방법!">
+      <area shape="rect" coords="41,596,269,906" href="https://npswebzine.kr/html/vol201/sub04.php" target="_blank" title="꼬리에 꼬리를 무는 문화탐구. 트로트를 신드롬으로 만든 임영웅이라는 세계" alt="꼬리에 꼬리를 무는 문화탐구. 트로트를 신드롬으로 만든 임영웅이라는 세계">
+      <area shape="rect" coords="285,597,516,881" href="https://npswebzine.kr/html/vol201/sub05.php" target="_blank" title="모두의 여행. 초가을에 거닐기 좋은 완주&middot;전주" alt="모두의 여행. 초가을에 거닐기 좋은 완주&middot;전주">
+      <area shape="rect" coords="530,597,760,904" href="https://npswebzine.kr/html/vol201/sub06.php" target="_blank" title="연금엔씨네마. 영화 <오베라는 남자>로 읽는 국민연금 실업 크레딧" alt="연금엔씨네마. 영화 <오베라는 남자>로 읽는 국민연금 실업 크레딧">
+      <area shape="rect" coords="41,933,270,1239" href="https://npswebzine.kr/html/vol201/sub07.php" target="_blank" title="키워드로 보는 세상. 더 길게 더 멀리 보는 튼튼한 노후 자금 준비법" alt="키워드로 보는 세상. 더 길게 더 멀리 보는 튼튼한 노후 자금 준비법">
+      <area shape="rect" coords="285,932,515,1218" href="https://npswebzine.kr/html/vol201/sub08.php" target="_blank" title="헬스 앤 라이프. 계절성 우울증 예방법" alt="헬스 앤 라이프. 계절성 우울증 예방법">
+      <area shape="rect" coords="531,932,761,1241" href="https://npswebzine.kr/html/vol201/sub09.php" target="_blank" title="오늘의 레시피. 영양 듬뿍 담은 '송이버섯솥밥' 레시피" alt="오늘의 레시피. 영양 듬뿍 담은 '송이버섯솥밥' 레시피">
+      <area shape="rect" coords="41,1399,760,1578" href="https://npswebzine.kr/html/vol201/sub10.php" target="_blank" title="독자목소리. 독자 여려분의 의견을 들려주세요." alt="독자목소리. 독자 여려분의 의견을 들려주세요.">
+      <area shape="rect" coords="41,1604,759,1783" href="https://npswebzine.kr/html/vol201/sub11.php" target="_blank" title="빈칸퀴즈. 웹진을 읽고 빈칸을 맞혀주세요!" alt="빈칸퀴즈. 웹진을 읽고 빈칸을 맞혀주세요!">
+      <area shape="rect" coords="40,1808,759,1989" href="https://npswebzine.kr/html/vol201/sub12.php" target="_blank" title="가을호 최애 콘텐츠를 뽑아라!" alt="가을호 최애 콘텐츠를 뽑아라!">
+      <area shape="rect" coords="41,2013,760,2192" href="https://npswebzine.kr/html/vol201/sub13.php" target="_blank" title="구독자 뽑기 이벤트. 행운의 사다리타기" alt="구독자 뽑기 이벤트. 행운의 사다리타기">
+      <area shape="rect" coords="41,2219,759,2398" href="https://npswebzine.kr/html/vol201/sub14.php" target="_blank" title="국민연금을 맞혀라! OX퀴즈" alt="국민연금을 맞혀라! OX퀴즈">
     </map>
 
 </body>

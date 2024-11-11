bdec0772ca499344b6e5e99796ee00a879009e22
puritechlab <pub@puritechlab.com>
2024-11-11 17:00:21 +0900

    sub09 pc edit 17:00

--- html/vol202/sub09.php
+++ html/vol202/sub09.php
@@ -34,71 +34,98 @@
 			<div class="section02 wow fadeBigInUp">
 				<div class="content">
 					<div class="content01 fadeBigInUp">
-						<div class="subtitle_top">초가을의 정취를 품은<br class="mobile_display"> 송이버섯</div>
-						<div class="title pc_display">영양 듬뿍 담은 ‘송이버섯솥밥’ 레시피</div>
+						<div class="subtitle_top">나야 굴~, 바다의 우유</div>
+						<div class="title pc_display">영양이 오동통, 굴전 레시피</div>
 						<div class="title mobile_display">영양 듬뿍 담은 <br>‘송이버섯솥밥’ 레시피</div>
 
 						<div class="subcopy">
-							추석이 다가오면 마트 진열장에서 자연산 송이버섯을 만날 수 있다. 맛깔스러운 자태에 이끌려 장바구니에 집어넣다가 1kg당 수십만 원을 호가하는 가격에 입이 떡 벌어진다. 하지만 자연산 송이의 냄새를 한번 맡고 나서는 비싼 가격에 고개가 끄덕여진다. 다른 버섯과 달리 강렬하고 향긋한 솔향이 몸속으로 퍼져 나가 마치 자연을 거니는 듯한 기분이 들게 한다.
+							통통하게 살이 오른 12월의 굴은 풍부한 영양분을 함유하고 깊고 진한 바다 향기를 머금어 가장 맛있게 즐길 수 있다. 동서양을 막론하고 사랑받는 식재료인 굴에 관해 이야기한다.
 						</div>
 					</div><!-- //.content01 -->
 
 					<div class="content02 fadeBigInUp">
 						<div class="img_box"><img src="img/sub09/sub09_01.jpg"></div>
-						<div class="subtitle">식물과 공생하는 버섯</div>
+						<div class="subtitle">천연 강장제라 불러주세요</div>
 						<br class="pc_display">
 						<div class="text">
-							송이버섯을 포함한 모든 버섯은 생태계와 공생하는 고마운 곰팡이다. 곰팡이는 식물이 남긴 영양분을 먹고 자라는데, 자연산 송이버섯의 경우 소나무 뿌리에 붙어 더부살이하며 소나무가 광합성으로 만든 탄수화물을 먹고 자란다. 송이버섯의 포자가 소나무 영양분을 골라먹고 무럭무럭 자라는 과정에서 흙 속의 수분과 양분도 송이버섯 안으로 흡수된다. 전문가들은 이때 버섯이 흡수한 무기물, 인, 질소 등 양분이 소나무에 공급되어 뿌리를 보호하는 역할을 하는 것으로 추측하고 있다.<br>송이버섯에 번식의 기회를 부여한 소나무, 그런 소나무에 수분과 양분으로 고마움을 돌려주는 송이버섯. 이 둘의 독특한 공생관계가 명확히 밝혀지지 않았지만, 확실한 건 버섯은 식물들과 공생관계를 이루고, 나무의 생장을 돕는 등 생태계에서 중요한 역할을 담당하는 고마운 식재료라는 점이다.
+							서양인은 예부터 해산물을 날것으로 먹지 않았지만, 굴만은 날것으로 즐겨 먹었다. 로마 황제 카이사르, 프랑스 황제 나폴레옹, 독일 철혈재상 비스마르크, 프랑스 소설가 뒤마, 미국 대통령 아이젠하워 등 수많은 위인이 굴을 좋아했다고 한다. 매일 아침 식사로 굴 50개를 먹었다는 카사노바 이야기도 전해진다. 맛도 좋지만, 이들이 굴에 빠진 이유는 무엇보다 자양강장에 탁월하다고 여겼기 때문이다. <br>
+							굴이 남성 활력에 좋다는 말은 입증된 사실이다. 굴에 함유된 풍부한 아연 성분이 건강한 정자를 생성하고, 남성 호르몬인 테스토스테론 분비에도 도움을 준다. 이 밖에 굴에는 양기를 북돋는 단백질과 피로 회복에 좋은 글리코겐이 포함돼 스태미나에도 효과적이다. <br>
+							그렇다면 굴은 남성에게만 좋은 식재료일까? 굴은 고대 이집트 여왕 클레오파트라도 즐겨 먹었다는 이야기가 전해질 만큼 여성에게도 인기 있는 식재료이다. 굴에는 항산화 물질이 들어 있어 피부 노화 방지에 효과가 좋고, 열량이 낮아 다이어트에도 도움을 준다. 실제로 최근에는 굴 추출물이 포함된 화장품 등이 다양하게 출시되는 등 각종 뷰티 산업에서도 굴을 활동하고 있다. 이 밖에 구리 성분도 함유해 철분이 체내에 잘 흡수되도록 돕기 때문에 빈혈 관리에도 좋다.
 						</div>
 					</div><!-- //.content02 -->
 					
 					<div class="content03 fadeBigInUp">
-						<div class="subtitle">뛰어난 효능의 ‘금’송이</div>
+						<div class="subtitle">한국에서만 저렴하게 먹을 수 있어요</div>
 						<br class="pc_display">
 						<div class="text">
-							허준의 『동의보감』에는 ‘송이는 성분이 고르고 맛이 달며, 독은 없고 소나무 냄새를 포함해 향이 뛰어나다. 산중에 오래된 소나무 밑에서 소나무 기운을 받아 자라는 것으로 버섯 중 으뜸이다’라고 적혀 있다. 자연산 송이버섯이 오래전부터 버섯의 왕이었음을 알 수 있는 대목이다.<br>
-							송이버섯은 맛과 향뿐 아니라 효능도 뛰어난데, 혈중 콜레스테롤을 억제해 위장병, 당뇨병, 동맥경화, 고지혈증 등을 예방하는 데 도움을 준다. 한의학적으로는 위장의 기능을 북돋아 주고 소화력이 약한 사람에게 효과적이라고 알려졌다.<br>
-							송이버섯은 소나무, 미생물, 토양인자, 기상 환경 등 여러 조건이 맞아떨어져야만 생장할 수 있어 자연산으로만 채취할 수 있다. 우리나라에서는 경북 울진, 영주, 봉화 지방과 강원 강릉, 양양 지방에서 주로 나며, 산림청의 허가를 받은 채취원만이 자연산 송이버섯을 채취할 수 있다. 본인 소유나 임차하지 않은 산에서 송이버섯을 채취하면 5년 이하 징역 또는 5천만 원 이하 벌금형에 처할 수 있으니 등산 중 송이버섯을 본다고 해도 함부로 채취해서는 안 된다.
+							한국에 온 외국인들은 겨울이면 식당에서 밑반찬으로 나온 굴을 보고 눈이 휘둥그레진다. 외국에서는 굴 1개당 최소 2~3천 원이기 때문에 굴을 고급 식재료로 인식한다. 그런데 한국에서는 굴 1kg을 2만 원 안팎에 구매할 수 있어 쉽게 접할 수 있다. <br>
+							한국에서 굴이 저렴한 이유는 풍부한 생산량 덕분이다. 서양에서는 굴을 양식하는 데 2년이 걸리지만, 한국은 1년이면 상품성 높은 굴을 수확할 수 있다. 서해와 남해가 굴곡진 형태의 리아스식 해안으로 이루어져 굴 성장에 매운 좋은 환경을 갖췄기 때문이다. <br>
+							국내 굴 양식은 1923년 경남 가덕도 연안의 간석지에서 최초 시작된 이래 1960년대 경남 통영에서 본격화되었고 이후 서해와 남해로 확대되며 꾸준히 늘었다. 양식에 관한 여러 연구가 진행되며 생산량이 증가했고, 가격이 하락함과 동시에 대중화에 성공한다. 이렇게 식탁에 오르게 된 굴은 초고추장에 찍어 먹거나 굴국밥 혹은 굴전으로 먹기도 하고 굴보쌈으로 활용하는 등 다양한 방식으로 조리된다. <br>
+							굴 섭취 시 주의해야 될 점이 있는데, 바로 위장 염증을 일으키는 노로바이러스이다. 주로 날 것의 어패류 섭취나 오염된 손과의 접촉을 통해 전파된다. 따라서 개인 위생을 철저히 하고, 섭취하려는 어패류를 깨끗이 세척하거나 85~90℃ 온도에서 1분 이상 가열해 익혀 먹으면 바이러스를 예방할 수 있다. 무엇보다 굴 제품 포장지에 ‘가열조리용’ 혹은 ‘익혀 먹는’ 등의 표시가 있다면 반드시 가열해 먹어야 한다.
 						</div>
 					</div><!-- //.content03 -->
 
 					<div class="content04">
 						<div class="item_box">
 							<div class="item item01">
-								<div class="item_title">자연산 송이버섯의 조건</div>
+								<div class="item_title">신선한 굴의 조건</div>
 								<div class="img_box"><img src="img/sub09/sub09_02.jpg"></div>
 								<ul>
-									<li><img src="img/sub09/sub09_item_num1.svg"><span>송이버섯 특유의 솔향이 강하다.</span></li>
-									<li><img src="img/sub09/sub09_item_num2.svg"><span>갓이 완전히 펴지지 않았다.</span></li>
-									<li><img src="img/sub09/sub09_item_num3.svg"><span>줄기가 짧고 굵으며 무겁고 단단하다.</span></li>
+									<li><img src="img/sub09/sub09_item_num1.svg"><span>색이 선명하고 탱탱하다.</span></li>
+									<li><img src="img/sub09/sub09_item_num2.svg"><span>껍데기가 닫혔다.</span></li>
+									<li><img src="img/sub09/sub09_item_num3.svg"><span>깨끗한 수조에 보관되어 있다.</span></li>
+									<li><img src="img/sub09/sub09_item_num4.svg"><span>알맹이가 유백색을 띤다.</span></li>
+									<li><img src="img/sub09/sub09_item_num5.svg"><span>알맹이의 검은 테두리가 선명하고 뚜렷하다.</span></li>
+									<li><img src="img/sub09/sub09_item_num6.svg"><span>냄새가 나지 않는다.</span></li>
 								</ul>
 							</div>
 							<div class="item item02">
-								<div class="item_title">자연산 송이버섯 보관 TIP</div>
+								<div class="item_title">굴 손질법</div>
 								<div class="img_box"><img src="img/sub09/sub09_03.jpg"></div>
-								<div class="pc_display">
+								<div>
 									<ul>
-										<li><img src="img/sub09/sub09_item2_num1.svg"><span>송이버섯은 씻지 말고 포자와 주변에 붙은 이물질을 턴다.</span></li>
-										<li><img src="img/sub09/sub09_item2_num2.svg"><span>신문지 또는 한지로 감싸 냉장실에 보관한다.</span></li>
-										<li><img src="img/sub09/sub09_item2_num3.svg"><span>랩으로 진공 포장 후 냉동하면 3개월간 보관할 수 있다.</span></li>
+										<li><img src="img/sub09/sub09_item2_num1.svg"><span>껍데기째 있는 굴은 칫솔이나 솔로 구석구석 씻는다.</span></li>
+										<li><img src="img/sub09/sub09_item2_num2.svg"><span>알맹이는 소금물로 씻어 이물질을 제거한다.</span></li>
+										<li><img src="img/sub09/sub09_item2_num3.svg"><span>생굴을 무즙에 재우면 비린내 제거에 좋다.</span></li>
 									</ul>
 								</div>
-								<div class="mobile_display">
+								<!-- <div class="mobile_display">
 									<ul>
 										<li><img src="img/sub09/sub09_item2_01.svg"></li>
 										<li><img src="img/sub09/sub09_item2_02.svg"></li>
 										<li><img src="img/sub09/sub09_item2_03.svg"></li>
 									</ul>
-								</div>
+								</div> -->
+							</div>
+						</div>
+
+						<div class="item_box2">
+							<div class="bg"><img src="img/sub09/store-oyster-bg.svg"></div>
+							<div class="item_cont">
+								<div class="item2_title">굴 보관 방법</div>
+								<ul>
+									<li>
+										<div class="img"><img src="img/sub09/store-oyster1.svg"></div>
+										<div class="text">
+											소금물에 잠시 담갔다가 물기를 제거한 후 밀폐용기나 채반에 두고 그 아래 얼음을 깔아 냉장 보관한다. 반드시 2~5일 이내에 먹는다.
+										</div>
+									</li>
+									<li>
+										<div class="img"><img src="img/sub09/store-oyster2.svg"></div>
+										<div class="text">
+											소금물에 담가 0~4℃에 보관하고 최대한 빠르게 먹는다.
+										</div>
+									</li>
+									<li>
+										<div class="img"><img src="img/sub09/store-oyster3.svg"></div>
+										<div class="text">
+											냉장 보관하며 최대한 빠른 시일 내에 섭취한다. 
+										</div>
+									</li>
+								</ul>
 							</div>
 						</div>
 					</div><!-- //content04 -->
-
-					<div class="content05 fadeBigInUp">
-						<div class="text">
-							자연산 송이는 생으로 먹는 것이 일반적이지만, 더 풍부한 향을 느끼고 싶거나, 귀한 손님에게 대접하고자 한다면 송이버섯솥밥을 추천한다. 가을을 담은 송이버섯솥밥 레시피를 소개한다. 
-						</div>
-					</div><!-- //.content03 -->
 				</div><!-- //.content -->
 			</div><!-- //.section02 -->
 
@@ -125,8 +152,7 @@
 						<li class="item1 fadeBigInUp">
 							<div class="item_title"><img src="img/sub09/sub09_06.svg"></div>
 							<div class="item_text pc_display">
-								쌀 2컵, 송이버섯 2개, 은행 15알, 실파 5대, 다시마 육수 2컵, 간장 3큰술, 맛술 1큰술, <br>
-								고춧가루 1작은술, 다진 마늘 1/2작은술, 참기름 1큰술, 통깨 약간
+								굴 1팩(250g), 계란 2개, 실파 7대, 밀가루 2큰술 반, 소금 1작은술, 홍고추와 풋고추 1개, 후추
 							</div>
 							<div class="item_text mobile_display">
 								쌀 2컵, 송이버섯 2개, 은행 15알, 실파 5대, <br>
@@ -135,7 +161,8 @@
 								참기름 1큰술, 통깨 약간
 							</div>
 							<div class="item_img">
-								<img src="img/sub09/ingred01.png" class="ingred1">
+								<img src="img/sub09/ingred-full.png">
+								<!-- <img src="img/sub09/ingred01.png" class="ingred1">
 								<img src="img/sub09/ingred02.png" class="ingred2">
 								<img src="img/sub09/ingred03.png" class="ingred3">
 								<img src="img/sub09/ingred04.png" class="ingred4">
@@ -145,59 +172,47 @@
 								<img src="img/sub09/ingred08.png" class="ingred8">
 								<img src="img/sub09/ingred09.png" class="ingred9">
 								<img src="img/sub09/ingred10.png" class="ingred10">
-								<img src="img/sub09/ingred11.png" class="ingred11">
+								<img src="img/sub09/ingred11.png" class="ingred11"> -->
 							</div>
 						</li>
 
 						<li class="item2 fadeBigInUp">
-							<div class="item_title"><img src="img/sub09/sub09_08.svg"></div>
+							<div class="item_title"><img src="img/sub09/step-num1.svg"></div>
 							<div class="item_text">
-								쌀은 씻어 30분간 불린 후 체에 밭쳐 물기를 뺀다.<br>
-								<span>tip. 기호에 따라 찹쌀을 넣어도 좋다.</span>
+								그릇에 굴과 소금을 넣고 물을 부어 10분간 휴지한 후, 흐르는 물에 3~4번 헹군다.
 							</div>
 							<div class="item_img">
-								<img src="img/sub09/sub09_09-1.png" class="img1">
-								<img src="img/sub09/sub09_09-2.png" class="img2">
+								<img src="img/sub09/cooking01.png">
 							</div>
 						</li>
 
 						<li class="item3 fadeBigInUp">
-							<div class="item_title"><img src="img/sub09/sub09_10.svg"></div>
-							<div class="item_text">송이버섯은 결 대로 먹기 좋게 찢어 준비한다.</div>
+							<div class="item_title"><img src="img/sub09/step-num2.svg"></div>
+							<div class="item_text">고명으로 쓰일 홍고추와 풋고추, 곁들임으로 먹을 실파를 썬다.</div>
 							<div class="item_img">
-								<img src="img/sub09/sub09_11-4.png" class="img4">
-								<img src="img/sub09/sub09_11-2.png" class="img2">
-								<img src="img/sub09/sub09_11-3.png" class="img3">
-								<img src="img/sub09/sub09_11-5.png" class="img5">
-								<img src="img/sub09/sub09_11-6.png" class="img6">
+								<img src="img/sub09/cooking02.png">
 							</div>
 						</li>
 
 						<li class="item4 fadeBigInUp">
-							<div class="item_title"><img src="img/sub09/sub09_12.svg"></div>
-							<div class="item_text">냄비에 불린 쌀을 넣고 다시마 육수를 부은 후 버섯과 은행을 돌려 담고 끓인다.</div>
+							<div class="item_title"><img src="img/sub09/step-num3.svg"></div>
+							<div class="item_text">
+								계란 2개, 썰어 놓은 홍고추와 풋고추, 후추와 소금을 함께 섞어 계란물을 만든다. <br>
+								굴에 밀가루를 입히고 계란물에 넣는다.
+							</div>
 							<div class="item_img">
-								<img src="img/sub09/sub09_13-1.png" class="img1">
-								<img src="img/sub09/sub09_13-2.png" class="img2">
-								<img src="img/sub09/sub09_13-3.png" class="img3">
-								<img src="img/sub09/sub09_13-4.png" class="img4">
-								<img src="img/sub09/sub09_13-5.png" class="img5">
-								<img src="img/sub09/sub09_13-6.png" class="img6">
+								<img src="img/sub09/cooking03.png">
 							</div>
 						</li>
 
 						<li class="item5 fadeBigInUp">
-							<div class="item_title"><img src="img/sub09/sub09_14.svg"></div>
+							<div class="item_title"><img src="img/sub09/step-num4.svg"></div>
 							<div class="item_text">
-								간장, 맛술, 고춧가루, 다진 마늘, 참기름, 통깨를 섞어 양념 간장을 준비한다. <br class="pc_display">
-								이후 밥물이 끓어 오르면 약불로 줄여 15~20분간 익힌 후, 불을 끄고 5분 정도 뜸을 들이면 완성!
+								프라이팬에 식용유를 두른 후 계란물을 입힌 굴을 노릇노릇 부치면 완성!<br>
+								곁들임용 실파와 함께 굴전을 즐기길 추천한다.
 							</div>
 							<div class="item_img">
-								<img src="img/sub09/sub09_15-1.png" class="img1">
-								<img src="img/sub09/sub09_15-2.png" class="img2">
-								<img src="img/sub09/sub09_15-3.png" class="img3">
-								<img src="img/sub09/sub09_15-4.png" class="img4">
-								<img src="img/sub09/sub09_15-5.png" class="img5">
+								<img src="img/sub09/cooking04.png">
 							</div>
 						</li>
 					</ul>
